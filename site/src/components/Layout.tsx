bd575028675298863b2ac1e00919f0837e81d1d7
John Letey <johnletey@gmail.com>
2020-06-09 19:52:46 +0100

    site: header

--- site/src/components/Layout.tsx
+++ site/src/components/Layout.tsx
@@ -1,5 +1,7 @@
-import { useColorMode } from "@chakra-ui/core";
+import { useColorMode, Heading, Tag } from "@chakra-ui/core";
 import { useKeyBindings } from "../lib/key";
+import { Box, Flex, Icon, Button, Avatar, Text } from "@chakra-ui/core";
+import { version } from "../../../package.json";
 
 const Layout = ({ children }) => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -10,7 +12,37 @@ const Layout = ({ children }) => {
     },
   });
 
-  return <div>{children}</div>;
+  return (
+    <Box h="100vh">
+      <Flex mb={16} w="full">
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          pt={4}
+          pb={4}
+          maxW="1250px"
+          margin="0 auto"
+          w="full"
+          px={8}
+        >
+          <Flex justifyContent="center" alignItems="center">
+            <Text fontSize="4xl">Feather</Text>
+            <Button marginLeft={2} variant="outline" size="sm">
+              v{version}
+            </Button>
+          </Flex>
+          <Flex justifyContent="center" alignItems="center">
+            <Button variant="ghost" onClick={toggleColorMode}>
+              <Icon name={colorMode == "light" ? "moon" : "sun"} size="24px" />
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex margin="0 auto" direction="column" maxW="1250px" px={8}>
+        {children}
+      </Flex>
+    </Box>
+  );
 };
 
 export default Layout;
