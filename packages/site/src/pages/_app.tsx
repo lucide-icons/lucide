307ece32c56f1e867e363a79e91936f1a05a875b
John Letey <johnletey@gmail.com>
2020-08-29 22:16:38 +0100

    feat: Site (#1)
    * site: pull data from "icons" dir
    
    * site: display icons
    
    * site: remove redundant code
    
    * site: colour mode support
    
    * site: header
    
    * site: order imports
    
    * site: search
    
    * site: add toast when copying icon
    
    * site: styling
    
    * site: hero
    
    * fix: disable theme toggle transitions
    
    * feat: Use Yarn Workspaces
    
    * refactor: Update site deploy scripts
    
    * refactor: Remove dark mode for now
    
    * feat: Add site title
    
    * refactor: Fix warning and format
    
    * feat: Add dark mode back :eyes:
    
    * feat: Escape key to reset query
    
    * Fix by aelfric
    
    * Add Github link
    
    * Fix #40
    
    Co-authored-by: Eric Fennis <eric.fennis@gmail.com>

---
+++ packages/site/src/pages/_app.tsx
@@ -0,0 +1,47 @@
+import { CSSReset, ThemeProvider, ColorModeProvider } from "@chakra-ui/core";
+import { useRouter } from "next/router";
+import { QueryParamProvider } from "use-query-params";
+import customTheme from "../lib/theme";
+import Head from "next/head";
+
+const QueryProvider = ({ children }) => {
+  const router = useRouter();
+
+  const history = {
+    push: ({ search }: Location) =>
+      router.push({ search, pathname: router.pathname }),
+
+    replace: ({ search }: Location) =>
+      router.replace({ search, pathname: router.pathname }),
+  };
+
+  const location = {
+    search: router.asPath.replace(/[^?]+/u, ""),
+  } as Location;
+
+  return (
+    <QueryParamProvider history={history} location={location}>
+      {children}
+    </QueryParamProvider>
+  );
+};
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Featherity</title>
+      </Head>
+      <QueryProvider>
+        <ThemeProvider theme={customTheme}>
+          <ColorModeProvider>
+            <CSSReset />
+            <Component {...pageProps} />
+          </ColorModeProvider>
+        </ThemeProvider>
+      </QueryProvider>
+    </>
+  );
+};
+
+export default App;
