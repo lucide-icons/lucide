dbcd37b194f68c30bd324aa19ffd491f32bfffbd
Eric Fennis <eric.fennis@gmail.com>
2020-09-04 14:45:16 +0200

    Fix svg generation

--- src/createElement.js
+++ src/createElement.js
@@ -1,8 +1,8 @@
 const createElement = (tag, attrs, children = []) => {
-  const element = document.createElementNS(tag);
+  const element = document.createElementNS('http://www.w3.org/2000/svg', tag);
 
   Object.keys(attrs).forEach(name => {
-    element.setAttributeNS(name, attrs[name]);
+    element.setAttribute(name, attrs[name]);
   });
 
   if (children.length) {
