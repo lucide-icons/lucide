a45d25ee734a027e46976f0caff6ad672b196449
Eric Fennis <eric.fennis@endurance.com>
2020-10-31 22:55:16 +0100

    Add Logo

--- site/src/components/Layout.tsx
+++ site/src/components/Layout.tsx
@@ -1,7 +1,12 @@
 import { Box, Divider, Flex, Text, Link, Icon, useColorMode, useColorModeValue, IconButton } from "@chakra-ui/core";
 import { useKeyBindings } from "../lib/key";
 import { useRouter } from "next/router";
+import NextLink from "next/link"
 import { Moon, Sun } from 'lucide-react';
+import Logo from 'babel-loader!react-svg-loader?jsx=true!../../public/logo.svg';
+
+// console.log(Logo);
+
 
 const Layout = ({ children }) => {
   const router = useRouter();
@@ -39,16 +44,23 @@ const Layout = ({ children }) => {
           px={8}
         >
           <Flex justifyContent="center" alignItems="center">
-            <Text
-              fontSize="4xl"
-              onClick={() => setQuery("")}
-              style={{ cursor: "pointer" }}
-            >
-              Lucide
-            </Text>
+            <NextLink href="/" passHref>
+              <Link display="flex" breakout _hover={{textDecoration: 'none'}}>
+                <Icon boxSize={12} marginRight="8px">
+                  <Logo/>
+                </Icon>
+                <Text
+                  fontSize="40px"
+                  lineHeight="48px"
+                >
+                  Lucide
+                </Text>
+              </Link>
+            </NextLink>
           </Flex>
           <Flex justifyContent="center" alignItems="center">
-          <Link href="https://github.com/lucide-icons/lucide" isExternal style={{ fontSize: "18px", marginRight: '24px' }}>
+          <Link href="https://github.com/lucide-icons/lucide" isExternal marginRight={6} fontSize="xl">
+
             Github
           </Link>
           <IconButton
