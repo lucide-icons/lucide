21d8ac719f6632f412f0da6c48642f54ba714bec
Eric Fennis <eric.fennis@gmail.com>
2020-11-03 20:58:51 +0100

    Site: Add Logo (#121)
    * bump package
    
    * Add Logo
    
    * remove console
    
    * prettify it
    
    * add favicons and fix issue

--- site/src/components/Layout.tsx
+++ site/src/components/Layout.tsx
@@ -1,7 +1,9 @@
 import { Box, Divider, Flex, Text, Link, Icon, useColorMode, useColorModeValue, IconButton } from "@chakra-ui/core";
 import { useKeyBindings } from "../lib/key";
 import { useRouter } from "next/router";
+import NextLink from "next/link"
 import { Moon, Sun } from 'lucide-react';
+import Logo from 'babel-loader!react-svg-loader?jsx=true!../../public/logo.svg';
 
 const Layout = ({ children }) => {
   const router = useRouter();
@@ -39,16 +41,27 @@ const Layout = ({ children }) => {
           px={8}
         >
           <Flex justifyContent="center" alignItems="center">
-            <Text
-              fontSize="4xl"
-              onClick={() => setQuery("")}
-              style={{ cursor: "pointer" }}
-            >
-              Lucide
-            </Text>
+            <NextLink href="/" passHref>
+              <Link display="flex" _hover={{textDecoration: 'none'}}>
+                <Icon boxSize={12} marginRight="8px">
+                  <Logo/>
+                </Icon>
+                <Text
+                  fontSize="40px"
+                  lineHeight="48px"
+                >
+                  Lucide
+                </Text>
+              </Link>
+            </NextLink>
           </Flex>
           <Flex justifyContent="center" alignItems="center">
-          <Link href="https://github.com/lucide-icons/lucide" isExternal style={{ fontSize: "18px", marginRight: '24px' }}>
+          <Link
+            href="https://github.com/lucide-icons/lucide"
+            isExternal
+            marginRight={6}
+            fontSize="xl"
+          >
             Github
           </Link>
           <IconButton
