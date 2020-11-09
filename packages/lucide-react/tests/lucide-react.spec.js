131597e4a4663e264f2f2311dc59d3b6e1b755bb
Eric Fennis <eric.fennis@endurance.com>
2020-11-09 21:20:23 +0100

    Add tests

--- packages/lucide-react/tests/lucide-react.spec.js
+++ packages/lucide-react/tests/lucide-react.spec.js
@@ -2,14 +2,23 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Grid } from '..'
 
-// const Component = () => (
-//   <div>hoi</div>
-// )
-
 describe('Using lucide icon components', () => {
   it('should render an component', () => {
     const component = renderer.create(
       <Grid/>,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should adjust the size, stroke color and stroke width', () => {
+    const component = renderer.create(
+      <Grid
+        size={48}
+        stroke="red"
+        strokeWidth={4}
+      />,
     );
 
     let tree = component.toJSON();
