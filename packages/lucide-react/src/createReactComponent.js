10b42ce933abe2dec7dd84d62de101e11b3914a1
Eric Fennis <eric.fennis@gmail.com>
2020-11-21 23:51:51 +0100

    Add docs

--- packages/lucide-react/src/createReactComponent.js
+++ packages/lucide-react/src/createReactComponent.js
@@ -2,20 +2,21 @@ import { forwardRef, createElement } from 'react';
 import PropTypes from 'prop-types';
 
 export default (iconName, [tag, attrs, children]) => {
-  const Component = forwardRef(({ color = 'currentColor', size = 24, width = 2, ...rest }, ref) =>
-    createElement(
-      tag,
-      {
-        ref,
-        ...attrs,
-        width: size,
-        height: size,
-        color,
-        strokeWidth: width,
-        ...rest,
-      },
-      children.map(([childTag, childAttrs]) => createElement(childTag, childAttrs)),
-    ),
+  const Component = forwardRef(
+    ({ color = 'currentColor', size = 24, strokeWidth = 2, ...rest }, ref) =>
+      createElement(
+        tag,
+        {
+          ref,
+          ...attrs,
+          width: size,
+          height: size,
+          color,
+          strokeWidth,
+          ...rest,
+        },
+        children.map(([childTag, childAttrs]) => createElement(childTag, childAttrs)),
+      ),
   );
 
   Component.propTypes = {
