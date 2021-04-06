92ef4c58d718356015f663aa3374c49490d7faf1
SMAH1 <integral22003@yahoo.com>
2021-04-06 12:54:39 +0430

    Add lucide-angular

---
+++ packages/lucide-angular/projects/lucide-angular/icons-index.ts
@@ -0,0 +1,4 @@
+export * from './build/icons-index';
+
+import * as icons from './build/icons-index';
+export { icons };
---
+++ packages/lucide-angular/projects/lucide-angular/src/lib/lucide-angular.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, ModuleWithProviders, Optional } from '@angular/core';
+import { LucideAngularComponent } from './lucide-angular.component';
+import { Icons } from './icons.provider';
+import { IconData  } from '../../lucide';
+
+@NgModule({
+  declarations: [LucideAngularComponent],
+  exports: [LucideAngularComponent]
+})
+export class LucideAngularModule {
+    constructor(@Optional() private icons: Icons) {
+        if (!this.icons) {
+            throw new Error(
+                `No icon provided. Make sure to use 'LucideAngularModule.pick({ ... })' when importing the module\n`
+            );
+        }
+    }
+
+    static pick(icons: { [key: string]: IconData }): ModuleWithProviders<LucideAngularModule> {
+        return {
+            ngModule: LucideAngularModule,
+            providers: [
+                { provide: Icons, multi: true, useValue: icons }
+            ]
+        };
+    }
+}
