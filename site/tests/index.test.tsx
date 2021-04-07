262f9063226c4bae58127c679ac73429b59909d6
Eric Fennis <eric.fennis@gmail.com>
2021-04-07 22:27:09 +0200

    Fix site

--- site/tests/index.test.tsx
+++ site/tests/index.test.tsx
@@ -1,10 +1,10 @@
 import { act, fireEvent, screen } from '@testing-library/react';
-import Index from '../pages/index';
+import Index from '../src/pages/index';
 import React from 'react';
 import { render } from './test-utils';
-import { getAllData } from '../lib/icons';
+import { getAllData } from '../src/lib/icons';
 
-import App from '../pages/_app';
+import App from '../src/pages/_app';
 
 describe('App', () => {
   it('renders without crashing', () => {
