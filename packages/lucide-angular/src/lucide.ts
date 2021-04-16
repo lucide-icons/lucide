e814cf45e6d306d6aa511c9a49159b38fcdfe6d0
Eric Fennis <eric.fennis@gmail.com>
2021-04-17 01:09:23 +0200

    Move files

--- packages/lucide-angular/src/lucide.ts
+++ packages/lucide-angular/src/lucide.ts
@@ -1,5 +1,3 @@
-export * from './lib/lucide-angular.component';
-export * from './lib/lucide-angular.module';
 import * as icons from './icons';
 export * from './icons';
 export * from './helpers/create-element';
