b71ae1e05536f4367aecfbe245507456289c0301
Eric Fennis <eric.fennis@gmail.com>
2021-04-17 22:00:21 +0200

    simplify exports

--- packages/lucide-angular/src/index.ts
+++ packages/lucide-angular/src/index.ts
@@ -1,3 +1,7 @@
+import * as icons from './icons';
+
 export * from './lib/lucide-angular.component';
 export * from './lib/lucide-angular.module';
-export * from './lucide';
+export * from './helpers/create-element';
+export * from './icons';
+export { icons };
--- packages/lucide-angular/src/lucide.ts
+++
@@ -1,5 +0,0 @@
-import * as icons from './icons';
-export * from './icons';
-export * from './helpers/create-element';
-
-export { icons };
