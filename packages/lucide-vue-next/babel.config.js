07196b6f069a01a2a2757aeaa9f00c14214e7184
Eric Fennis <eric.fennis@gmail.com>
2021-05-03 23:34:54 +0200

    Fix es module build

--- packages/lucide-vue-next/babel.config.js
+++ packages/lucide-vue-next/babel.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   presets: [
     [
-      '@babel/preset-env',
+      '@babel/env',
       {
-        targets: {
-          node: 'current',
-        },
+        loose: true,
+        modules: false,
       },
     ],
   ],
