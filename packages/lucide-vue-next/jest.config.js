4b183d7fe7390d336ea0843603e912d3a2d4ebda
Eric Fennis <eric.fennis@gmail.com>
2021-05-20 21:24:54 +0200

    Add vue 3 package  (#293)
    * init vue next package
    
    * Refactor naming
    
    * adjust readme
    
    * add typescript support
    
    * Fix es module build
    
    * Bump alpha version
    
    * Fix size property not working
    
    * Fix issue with default attributes in this PR
    
    * small fixes
    
    * Update README.md
    
    * Fix peer dep
    
    * Add return
    
    * update release workflow
    
    Co-authored-by: AdamSGit <adamelio@protonmail.com>

---
+++ packages/lucide-vue-next/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  verbose: true,
+  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
+  moduleFileExtensions: ['js'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': 'vue-jest',
+  },
+  transformIgnorePatterns: [`/node_modules`],
+  snapshotSerializers: ['jest-serializer-vue'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
