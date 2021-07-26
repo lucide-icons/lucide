9de205ce846cb5856a9cc63c3e4344d2b458bf83
Billiam <Billiam@users.noreply.github.com>
2021-07-26 13:12:13 -0500

    Build multiple font weights in action (#355)
    * Build multiple font thicknesses
    
    * Fontcustom empties directory during compilation

--- scripts/outline_svg.js
+++ scripts/outline_svg.js
@@ -3,21 +3,34 @@ const outlineStroke = require('svg-outline-stroke');
 const { parse, stringify } = require('svgson');
 
 const inputDir = `./icons/`;
-const outputDir = `./converted_icons/`;
+const outputDirs = {
+  'converted_icons_200': '1',
+  'converted_icons_300': '1.5',
+  'converted_icons': '2',
+  'converted_icons_500': '2.5',
+  'converted_icons_600': '3'
+}
 
 async function init() {
   try {
+    for (const directory of Object.keys(outputDirs)) {
+      await fs.mkdir(`./${directory}`);
+    }
+
     const files = await fs.readdir(inputDir);
     for (const file of files) {
       const icon = await fs.readFile(`${inputDir}${file}`);
       const scaled = await parse(icon.toString(), {
         transformNode: transformForward,
       });
-      const outlined = await outlineStroke(stringify(scaled));
-      const outlinedWithoutAttrs = await parse(outlined, {
-        transformNode: transformBackwards,
-      });
-      await fs.writeFile(`${outputDir}${file}`, stringify(outlinedWithoutAttrs));
+      for (const [directory, width] of Object.entries(outputDirs)) {
+        scaled.attributes['stroke-width'] = width;
+        const outlined = await outlineStroke(stringify(scaled));
+        const outlinedWithoutAttrs = await parse(outlined, {
+          transformNode: transformBackwards,
+        });
+        await fs.writeFile(`./${directory}/${file}`, stringify(outlinedWithoutAttrs));
+      }
     }
   } catch (err) {
     console.log(err);
