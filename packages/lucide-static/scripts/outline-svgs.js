18b506864c4384b72959dfb85dc421688c82edbf
Eric Fennis <eric.fennis@gmail.com>
2021-08-08 21:22:29 +0200

    Add lucide static

---
+++ packages/lucide-static/scripts/outline-svgs.js
@@ -0,0 +1,52 @@
+const { promises: fs } = require('fs');
+const outlineStroke = require('svg-outline-stroke');
+const { parse, stringify } = require('svgson');
+
+const inputDir = `./icons/`;
+const outputDir = `./converted_icons/`;
+
+function transformForward(node) {
+  if (node.name === 'svg') {
+    return {
+      ...node,
+      attributes: {
+        ...node.attributes,
+        width: 960,
+        height: 960,
+      },
+    };
+  }
+  return node;
+}
+
+function transformBackwards(node) {
+  if (node.name === 'svg') {
+    const { width, height, ...attributes } = node.attributes;
+    return {
+      ...node,
+      attributes,
+    };
+  }
+  return node;
+}
+
+async function init() {
+  try {
+    const files = await fs.readdir(inputDir);
+    for (const file of files) {
+      const icon = await fs.readFile(`${inputDir}${file}`);
+      const scaled = await parse(icon.toString(), {
+        transformNode: transformForward,
+      });
+      const outlined = await outlineStroke(stringify(scaled));
+      const outlinedWithoutAttrs = await parse(outlined, {
+        transformNode: transformBackwards,
+      });
+      await fs.writeFile(`${outputDir}${file}`, stringify(outlinedWithoutAttrs));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
