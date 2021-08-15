733ce805eee2e16f87e6461b1a74cc654ab3f8ee
Eric Fennis <eric.fennis@gmail.com>
2021-08-15 20:46:42 +0200

    Fix eslint

--- site/next-env.d.ts
+++ site/next-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module "*.svg" {
-  const content: any;
-  export default content;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
