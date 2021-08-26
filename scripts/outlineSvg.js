9a04665ad2d7d2c41ab3b1296e0d8f2a926db922
Eric Fennis <eric.fennis@endurance.com>
2021-08-26 13:22:20 +0200

    refactor workflows

--- scripts/outlineSvg.js
+++ scripts/outlineSvg.js
@@ -1,43 +1,19 @@
-const { promises: fs } = require('fs');
-const outlineStroke = require('svg-outline-stroke');
-const { parse, stringify } = require('svgson');
+import { promises as fs } from 'fs';
+import outlineStroke from 'svg-outline-stroke';
+import { parse, stringify } from 'svgson'; // eslint-disable-line import/no-extraneous-dependencies
+
+// const { promises: fs } = require('fs');
+// const outlineStroke = require('svg-outline-stroke');
+// const { parse, stringify } = require('svgson');
 
 const inputDir = `./icons/`;
 const outputDirs = {
-  'converted_icons_200': '1',
-  'converted_icons_300': '1.5',
-  'converted_icons': '2',
-  'converted_icons_500': '2.5',
-  'converted_icons_600': '3'
-}
-
-async function init() {
-  try {
-    for (const directory of Object.keys(outputDirs)) {
-      await fs.mkdir(`./${directory}`);
-    }
-
-    const files = await fs.readdir(inputDir);
-    for (const file of files) {
-      const icon = await fs.readFile(`${inputDir}${file}`);
-      const scaled = await parse(icon.toString(), {
-        transformNode: transformForward,
-      });
-      for (const [directory, width] of Object.entries(outputDirs)) {
-        scaled.attributes['stroke-width'] = width;
-        const outlined = await outlineStroke(stringify(scaled));
-        const outlinedWithoutAttrs = await parse(outlined, {
-          transformNode: transformBackwards,
-        });
-        await fs.writeFile(`./${directory}/${file}`, stringify(outlinedWithoutAttrs));
-      }
-    }
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-init();
+  converted_icons_200: '1',
+  converted_icons_300: '1.5',
+  converted_icons: '2',
+  converted_icons_500: '2.5',
+  converted_icons_600: '3',
+};
 
 function transformForward(node) {
   if (node.name === 'svg') {
@@ -63,3 +39,44 @@ function transformBackwards(node) {
   }
   return node;
 }
+
+async function init() {
+  console.time('icon outliner');
+  try {
+    const createDirectories = Object.keys(outputDirs).map(directory => fs.mkdir(`./${directory}`));
+    await Promise.all(createDirectories);
+
+    const icons = await fs.readdir(inputDir);
+    const parsedIconNodes = await Promise.all(
+      icons.map(async file => {
+        const iconContent = await fs.readFile(`${inputDir}${file}`);
+        const iconNode = await parse(iconContent.toString(), {
+          transformNode: transformForward,
+        });
+        return [file, iconNode];
+      }),
+    );
+
+    await Promise.all(
+      Object.entries(outputDirs).map(async ([directory, width]) => {
+        await Promise.all(
+          parsedIconNodes.map(async ([file, iconNode]) => {
+            iconNode.attributes['stroke-width'] = width;
+            const outlined = await outlineStroke(stringify(iconNode));
+            const outlinedWithoutAttrs = await parse(outlined, {
+              transformNode: transformBackwards,
+            });
+
+            await fs.writeFile(`./${directory}/${file}`, stringify(outlinedWithoutAttrs));
+          }),
+        );
+      }),
+    );
+
+    console.timeEnd('icon outliner');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
