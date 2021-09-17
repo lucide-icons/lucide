f70a0e19458fe54d1d059929e905908b4bb29cf2
Eric Fennis <eric.fennis@endurance.com>
2021-09-17 16:23:17 +0200

    Add default classnames, fix #317

--- packages/lucide-vue-next/src/createVueComponent.js
+++ packages/lucide-vue-next/src/createVueComponent.js
@@ -1,15 +1,24 @@
 import { h } from 'vue';
 import defaultAttributes from './defaultAttributes';
 
+/**
+ * Converts string to KebabCase
+ * Copied from scripts/helper. If anyone knows how to properly import it here
+ * then please fix it.
+ *
+ * @param {string} string
+ * @returns {string} A kebabized string
+ */
+export const toKebabCase = string => string.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
+
 const createVueComponent = (iconName, iconNode) => (props, context) =>
   h(
     'svg',
     {
       ...defaultAttributes,
-      ...{
-        width: props.size || defaultAttributes.width,
-        height: props.size || defaultAttributes.height,
-      },
+      width: props.size || defaultAttributes.width,
+      height: props.size || defaultAttributes.height,
+      class: ['lucide', `lucide-${toKebabCase(iconName)}`],
       ...context.attrs,
       ...props,
     },
