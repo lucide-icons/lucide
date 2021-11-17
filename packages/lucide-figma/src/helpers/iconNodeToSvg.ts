8cab99a2eeb3aa8883ee8230063840ef945c9740
Eric Fennis <eric.fennis@gmail.com>
2021-11-17 20:12:45 +0100

    New Figma Plugin setup (#434)
    * Update changes
    
    * Add new ui files
    
    * add new script files
    
    * process
    
    * fix build
    
    * try to fix the worker
    
    * try to implement rollup for figma plugin
    
    * bump flutter package version
    
    * add vite for figma-plugin
    
    * add vite
    
    * add vite
    
    * Fix ext output
    
    * make plugin work!
    
    * Add Folder open icon
    
    Co-authored-by: Jan <72730682+JanTrichter@users.noreply.github.com>
    
    * add new fetcher
    
    * Build new api
    
    * add plugin fetcher
    
    * fix fetch icons
    
    * Fix search
    
    * Fix site build
    
    * update yarn.lock
    
    Co-authored-by: Jan <72730682+JanTrichter@users.noreply.github.com>

---
+++ packages/lucide-figma/src/helpers/iconNodeToSvg.ts
@@ -0,0 +1,11 @@
+import { createReactComponent } from "lucide-react";
+import { createElement } from "react";
+import { renderToString } from "react-dom/server";
+import { IconNode } from "../api/fetchIcons";
+
+const iconNodeToSvg = (iconName: string, iconNode : IconNode) => {
+  const IconComponent = createReactComponent(iconName, iconNode)
+  return  renderToString(createElement(IconComponent));
+}
+
+export default iconNodeToSvg
--- packages/lucide-figma/src/main.ts
+++ packages/lucide-figma/src/main.ts
@@ -1,9 +1,97 @@
-figma.showUI(__html__, { width: 300, height: 400 })
+import type { LucideIcons } from "./api/fetchIcons";
+import filterIcons from "./helpers/filterIcons";
 
-figma.ui.onmessage = ({name, svg}) => {
-  const icon = figma.createNodeFromSvg(svg)
-  icon.name = name
-  icon.x = figma.viewport.center.x
-  icon.y = figma.viewport.center.y
-  figma.currentPage.selection = [icon]
+figma.showUI(__uiFiles__.worker, { visible: false })
+
+let cachedIcons: LucideIcons
+
+const setResults = ({result, query, lucideIcons} : { result: SuggestionResults, query: string, lucideIcons: LucideIcons }) => {
+  const icons = Object.entries(lucideIcons.iconNodes);
+
+  const suggestions = filterIcons(icons, lucideIcons.tags, query.toLowerCase()).map(([name]) => ({
+    name,
+    icon: lucideIcons.svgs[name]
+  }))
+
+  result.setSuggestions(suggestions)
 }
+
+figma.parameters.on('input', async ({ parameters, key, query, result }) => {
+  if (key === 'icon-name') {
+    console.log('typ tpy', query);
+    cachedIcons = await figma.clientStorage.getAsync(`lucide-icons`)
+    console.log('cachedIcons', cachedIcons);
+
+    if(cachedIcons && cachedIcons.iconNodes && cachedIcons.tags) {
+      setResults({result, query, lucideIcons: cachedIcons})
+    }
+  }
+})
+
+const drawIcon = ({icon: {name, svg}}: any) => {
+  const min = 0
+  const max = 100
+  const randomPosition = () => Math.floor(Math.random() * (max - min + 1) + min)
+  const icon = figma.createNodeFromSvg(svg)
+
+  icon.name = name
+  icon.x = Math.round(figma.viewport.center.x + randomPosition())
+  icon.y = Math.round(figma.viewport.center.y + randomPosition())
+  figma.currentPage.selection = [icon]
+
+  // lock children
+  icon.children.forEach((vectorNode, key) => {
+    icon.children[key].locked = true
+  });
+}
+
+const setCachedIcons = async (pluginMessage: any) => {
+  if(pluginMessage.lucideIcons) {
+    await figma.clientStorage.setAsync(`lucide-icons`, pluginMessage.lucideIcons)
+  }
+}
+
+const getCachedIcons = async () => {
+  cachedIcons = await figma.clientStorage.getAsync(`lucide-icons`)
+
+  const response = { type: 'cachedIcons' }
+
+  if(cachedIcons) {
+    Object.assign(response, { cachedIcons })
+  }
+
+  figma.ui.postMessage(response)
+}
+
+getCachedIcons()
+
+figma.ui.onmessage = (event) => {
+  console.log(event, 'main');
+  switch (event.type) {
+    case "drawIcon":
+      drawIcon(event)
+      break;
+    case "getCachedIcons":
+      getCachedIcons()
+      break;
+
+    case "setCachedIcons":
+      setCachedIcons(event)
+      break;
+
+    case "close":
+      figma.closePlugin()
+      break;
+
+    default:
+      break;
+  }
+}
+
+figma.on('run', event => {
+  if(event.parameters) {
+    figma.ui.postMessage({ type: 'getSvg', iconName: event.parameters['icon-name'], cachedIcons })
+  } else {
+    figma.showUI(__uiFiles__.interface, { width: 300, height: 400 })
+  }
+})
