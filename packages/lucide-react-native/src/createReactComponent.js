88b0bddc44076880f20d5c275a189bdcf3ad0147
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-05-29 10:30:10 +0200

    Add `lucide-react-native` package
    Closes #394

---
+++ packages/lucide-react-native/src/createReactComponent.js
@@ -0,0 +1,47 @@
+import { forwardRef, createElement } from 'react';
+import PropTypes from 'prop-types';
+import NativeSvg from 'react-native-svg';
+import defaultAttributes from './defaultAttributes';
+
+/**
+ * Converts string to KebabCase
+ * Copied from scripts/helper. If anyone knows how to properly import it here
+ * then please fix it.
+ *
+ * @param {string} string
+ * @returns {string} A kebabized string
+ */
+export const toKebabCase = string => string.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
+
+export default (iconName, iconNode) => {
+  const Component = forwardRef(
+    ({ color = 'currentColor', size = 24, strokeWidth = 2, children, ...rest }, ref) =>
+      createElement(
+        NativeSvg.Svg,
+        {
+          ref,
+          ...defaultAttributes,
+          width: size,
+          height: size,
+          stroke: color,
+          strokeWidth,
+          className: `lucide lucide-${toKebabCase(iconName)}`,
+          ...rest,
+        },
+        [...iconNode.map(([tag, attrs]) => {
+          const uppercasedTag = tag.charAt(0).toUpperCase() + tag.slice(1);
+          return createElement(NativeSvg[uppercasedTag], attrs))
+        }, ...(children || [])],
+      ),
+  );
+
+  Component.propTypes = {
+    color: PropTypes.string,
+    size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    strokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  };
+
+  Component.displayName = `${iconName}`;
+
+  return Component;
+};
