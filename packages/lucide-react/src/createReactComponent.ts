24bf01770db093f52983be22174d213e5dd08c08
Louis Bailleau <louis.bailleau@free.fr>
2022-11-12 12:08:15 +0100

    Fix unit test for `lucide-react`

--- packages/lucide-react/src/createReactComponent.ts
+++ packages/lucide-react/src/createReactComponent.ts
@@ -1,12 +1,13 @@
 import React, { forwardRef, createElement, ReactSVG, SVGProps } from 'react';
 import PropTypes from 'prop-types';
-import { IconNode } from 'lucide'
+import { IconNode, DEFAULT_ATTRIBUTES } from 'lucide'
 
 export type SVGAttributes = Partial<SVGProps<SVGSVGElement>>
 
 export interface LucideProps extends SVGAttributes {
   size?: string | number
 }
+
 /**
  * Converts string to KebabCase
  * Copied from scripts/helper. If anyone knows how to properly import it here
@@ -17,14 +18,29 @@ export interface LucideProps extends SVGAttributes {
  */
 export const toKebabCase = (string: string): string => string.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
 
+/**
+ * Converts string to CamelCase
+ * Copied from scripts/helper. If anyone knows how to properly import it here
+ * then please fix it.
+ *
+ * @param {string} string
+ * @returns {string} A Camelized string
+ */
+export const toCamelCase = (string: string): string => string.replace(/-./g, x=>x[1].toUpperCase());
+
+const defaultAttrs = Object.keys(DEFAULT_ATTRIBUTES).reduce((acc, key)=> {
+  acc[toCamelCase(key)] = DEFAULT_ATTRIBUTES[key]
+  return acc
+}, {} as Record<string,string|number>)
+
 export function createReactComponent (iconName: string, [_tag, DEFAULT_ATTRIBUTES, nodeChildren=[]]: IconNode) {
-  const Component = forwardRef<SVGSVGElement, LucideProps>(
+    const Component = forwardRef<SVGSVGElement, LucideProps>(
     ({ color = 'currentColor', size = 24, strokeWidth = 2, children, ...rest }, ref) =>
       createElement(
         'svg',
         {
           ref,
-          ...DEFAULT_ATTRIBUTES,
+          ...defaultAttrs,
           width: size,
           height: size,
           stroke: color,
@@ -33,7 +49,7 @@ export function createReactComponent (iconName: string, [_tag, DEFAULT_ATTRIBUTE
           ...rest,
         },
         [
-          ...nodeChildren.map(([tag, attrs]) => createElement(tag, attrs)),
+          ...nodeChildren.map(([tag, attrs], index) => createElement(tag, { key: index, ...attrs })),
           ...(
             (Array.isArray(children) ? children : [children]) || []
           )
