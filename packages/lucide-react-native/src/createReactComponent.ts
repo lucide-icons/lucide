e6508af8f4ac4f0adaa39e326a7063f5715dc1c0
Eric Fennis <eric@dreamteam.nl>
2022-12-04 22:22:08 +0100

    Fix some types

--- packages/lucide-react-native/src/createReactComponent.ts
+++ packages/lucide-react-native/src/createReactComponent.ts
@@ -1,9 +1,8 @@
-import { forwardRef, createElement, ReactSVG } from 'react';
+import { forwardRef, createElement, ReactSVG, ReactNode, FunctionComponent } from 'react';
 import PropTypes from 'prop-types';
 import * as NativeSvg from 'react-native-svg';
 import defaultAttributes from './defaultAttributes';
 import type { SvgProps } from 'react-native-svg';
-// const NativeSvg = {}
 type IconNode = [elementName: keyof ReactSVG, attrs: Record<string, string>][]
 
 
@@ -27,10 +26,10 @@ const createReactComponent = (iconName: string, iconNode: IconNode) => {
         },
         [
           ...iconNode.map(([tag, attrs]) => {
-            const uppercasedTag = (tag.charAt(0).toUpperCase() + tag.slice(1));
-            return createElement(NativeSvg[uppercasedTag], attrs);
+            const upperCasedTag = (tag.charAt(0).toUpperCase() + tag.slice(1)) as keyof (typeof NativeSvg);
+            return createElement(NativeSvg[upperCasedTag] as FunctionComponent<Record<string, string>>, attrs);
           }),
-          ...(children || []),
+          ...(children as ReactNode[] || []),
         ],
       ),
   );
