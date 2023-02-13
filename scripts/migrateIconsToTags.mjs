cf68d54d08adca9079c63bb83d853d59d96bc1e2
Eric Fennis <eric.fennis@gmail.com>
2023-02-13 21:45:47 +0100

    Add tags to static

--- scripts/migrateIconsToTags.mjs
+++ scripts/migrateIconsToTags.mjs
@@ -1,15 +1,17 @@
 import path from 'path';
-import { writeFile, mergeArrays, getCurrentDirPath, readAllMetadata } from './helpers.mjs';
+import { writeFile, getCurrentDirPath, readAllMetadata } from './helpers.mjs';
 
-const currentDir = getCurrentDirPath(import.meta.url)
+const currentDir = getCurrentDirPath(import.meta.url);
 const ICONS_DIR = path.resolve(currentDir, '../icons');
 const icons = readAllMetadata(ICONS_DIR);
 
-const tags = Object.keys(icons).sort().reduce((acc, iconName, i) => {
-  acc[iconName] = icons[iconName].tags;
-  return acc;
-}, {});
+const tags = Object.keys(icons)
+  .sort()
+  .reduce((acc, iconName) => {
+    acc[iconName] = icons[iconName].tags;
+    return acc;
+  }, {});
 
 const tagsContent = JSON.stringify(tags, null, 2);
 
-writeFile(tagsContent, 'tags.json', path.resolve(currentDir, '..'));
+writeFile(tagsContent, 'tags.json', path.resolve(process.cwd()));
