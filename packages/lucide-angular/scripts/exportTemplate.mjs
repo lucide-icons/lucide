64ffb583284f9f884ac1f36c326df27b5b88cbf5
Karsa <karsa@karsa.org>
2023-02-15 09:18:06 +0100

    Almost complete rewrite of the Angular package

--- packages/lucide-angular/scripts/exportTemplate.mjs
+++ packages/lucide-angular/scripts/exportTemplate.mjs
@@ -2,10 +2,11 @@ export default ({ componentName, children }) => `
 import { IconData } from './types';
 import defaultAttributes from './constants/default-attributes';
 
-const ${componentName}: IconData  = [
-  'svg',
-  defaultAttributes,
-  ${JSON.stringify(children)}
+/* eslint-disable no-shadow-restricted-names */
+const ${componentName}: IconData = [
+    'svg',
+    defaultAttributes,
+    ${JSON.stringify(children)}
 ];
 
 export default ${componentName};
--- packages/lucide-angular/src/test.ts
+++ packages/lucide-angular/src/test.ts
@@ -4,23 +4,12 @@ import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
