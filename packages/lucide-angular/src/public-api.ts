29bd3ac0914f851d176df29026129841f46f724d
Karsa <karsa@karsa.org>
2023-02-15 10:14:23 +0100

    fixes icon build export file name

--- packages/lucide-angular/src/public-api.ts
+++ packages/lucide-angular/src/public-api.ts
@@ -1,7 +1,7 @@
-import * as icons from './icons';
+import * as icons from './icons/lucide-icons';
 
 export * from './lib/lucide-angular.component';
 export * from './lib/lucide-angular.module';
 export * from './helpers/create-element';
-export * from './icons';
+export * from './icons/lucide-icons';
 export { icons };
