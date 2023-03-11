60fcc0821671ecd0b450f9072c9394d906c15473
Jakob Guddas <github@jguddas.de>
2023-03-11 09:52:27 +0100

    chore: fixed nextjs path name parsing in production

--- site/src/pages/api/gh-icon/[data].tsx
+++ site/src/pages/api/gh-icon/[data].tsx
@@ -1,9 +1,14 @@
-import { renderToString } from 'react-dom/server';
 import SvgPreview from '../../../components/SvgPreview';
 
 export default async function handler(req, res) {
-  const src = Buffer.from(req.query.data.slice(0, -4), 'base64').toString('ascii');
+  // ReactDOMServer needs to be imported dynamically
+  // https://github.com/vercel/next.js/issues/43810
+  const ReactDOMServer = (await import('react-dom/server')).default;
+
+  const src = Buffer.from(req.url.split('/').at(-1).slice(0, -4), 'base64').toString('utf8');
+
+  const svg = Buffer.from(ReactDOMServer.renderToString(<SvgPreview src={src} showGrid />));
 
   res.setHeader('Content-Type', 'image/svg+xml');
-  res.status(200).end(renderToString(<SvgPreview src={src} showGrid />));
+  res.status(200).end(svg);
 }
