3ea3b3cd4ca8cd2b7c5cdac292117abc83ad7013
Jakob Guddas <github@jguddas.de>
2023-03-18 15:29:28 +0100

    fix: changed icon size base back to 24x24

--- site/src/components/SvgPreview/index.tsx
+++ site/src/components/SvgPreview/index.tsx
@@ -134,8 +134,8 @@ const SvgPreview = React.forwardRef<
     <svg
       ref={ref}
       xmlns="http://www.w3.org/2000/svg"
-      width={240}
-      height={240}
+      width={24}
+      height={24}
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
