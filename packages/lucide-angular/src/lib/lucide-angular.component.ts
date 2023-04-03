446873bda52b4382367326e2a46b24b69c6ad2fb
Karsa <contact@karsa.org>
2023-04-03 19:56:22 +0200

    Adds common API elements to the Angular package (#949)
    * Almost complete rewrite of the Angular package
    
    * Update tsconfig.spec.json
    
    * fixes icon build export file name
    
    * Updates Angular documentation with the new properties + provider injection
    
    * Update lucide-angular.md
    
    * refactored scaleStrokeWidth to be absoluteStrokeWidth to match other packages
    
    * removed codelyzer from devDeps + added flexible angular core dependencies
    
    * Deprecates createElement helper in favour of Renderer2 to support SSR
    
    ---------
    
    Co-authored-by: Karsa <karsa@karsa.org>

--- packages/lucide-angular/src/lib/lucide-angular.component.ts
+++ packages/lucide-angular/src/lib/lucide-angular.component.ts
@@ -1,72 +1,168 @@
-import { Component, ElementRef, Input, Inject, ChangeDetectorRef, OnChanges, SimpleChanges } from '@angular/core';
-import { Icons } from './icons.provider';
-import { IconData } from '../icons/types';
-import { createElement } from '../helpers/create-element';
+import {ChangeDetectorRef, Component, Renderer2, ElementRef, Inject, Input, OnChanges, SimpleChange} from '@angular/core';
+import {LucideIconData} from '../icons/types';
+import defaultAttributes from '../icons/constants/default-attributes';
+import {LUCIDE_ICONS, LucideIconProviderInterface} from './lucide-icon.provider';
+
+interface TypedChange<T> extends SimpleChange {
+    previousValue: T;
+    currentValue: T;
+}
+
+type LucideAngularComponentChanges = {
+    name?: TypedChange<string>;
+    img?: TypedChange<LucideIconData>;
+    color?: TypedChange<string>;
+    size?: TypedChange<number>;
+    strokeWidth?: TypedChange<number>;
+    absoluteStrokeWidth?: TypedChange<boolean>;
+};
+
+export function formatFixed(number: number, decimals = 3): string {
+    return parseFloat(number.toFixed(decimals)).toString(10);
+}
 
 @Component({
-  selector: 'lucide-angular, lucide-icon, i-lucide, span-lucide',
-  template: `<ng-content></ng-content>`,
-  styles: [`
-    :host {
-      display: inline-block;
-      fill: none;
-      stroke: currentColor;
-      stroke-width: 2;
-      stroke-linecap: round;
-      stroke-linejoin: round;
-      width: 24px;
-      height: 24px;
-    }
-  `]
+    selector: 'lucide-angular, lucide-icon, i-lucide, span-lucide',
+    template: '<ng-content></ng-content>',
 })
-
 export class LucideAngularComponent implements OnChanges {
-  @Input() name!: string;
-  @Input() img!: IconData;
+    @Input() class?: string;
+    @Input() name?: string;
+    @Input() img?: LucideIconData;
+    @Input() color: string = defaultAttributes.stroke;
+    _size: number = defaultAttributes.height;
+    get size(): number | string {
+        return this._size;
+    }
 
-  constructor(
-    @Inject(ElementRef) private elem: ElementRef,
-    @Inject(ChangeDetectorRef) private changeDetector: ChangeDetectorRef,
-    @Inject(Icons) private icons: Icons
-  ) { }
+    @Input() set size(value: string | number) {
+        this._size = this.parseNumber(value);
+    }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.name) {
-      // icons are provided as an array of objects because of "multi: true"
-      const icons = Object.assign({}, ...(this.icons as any as object[]));
-      const icoOfName = icons[this.toPascalCase(changes.name.currentValue)] || '';
+    _strokeWidth: number = defaultAttributes['stroke-width'];
+    get strokeWidth(): number | string {
+        return this._strokeWidth;
+    }
 
-      if (!icoOfName) {
-        console.warn(
-          `Icon not found: ${changes.name.currentValue}\n` +
-          `Please check icon name or \'lucide icon list\'`
+    @Input() set strokeWidth(value: string | number) {
+        this._strokeWidth = this.parseNumber(value);
+    }
+
+    @Input() absoluteStrokeWidth = false;
+    defaultSize: number;
+
+    constructor(
+        @Inject(ElementRef) private elem: ElementRef,
+        @Inject(Renderer2) private renderer: Renderer2,
+        @Inject(ChangeDetectorRef) private changeDetector: ChangeDetectorRef,
+        @Inject(LUCIDE_ICONS) private iconProviders: LucideIconProviderInterface[]
+    ) {
+        this.defaultSize = defaultAttributes.height;
+    }
+
+    ngOnChanges(changes: LucideAngularComponentChanges): void {
+        this.color = this.color ?? defaultAttributes.stroke;
+        this.size = this.parseNumber(this.size ?? this.defaultSize);
+        this.strokeWidth = this.parseNumber(
+            this.strokeWidth ?? defaultAttributes['stroke-width']
         );
-      } else {
-        const icoElement = createElement(icoOfName);
-        icoElement.setAttribute('stroke-width', 'inherit');
-        icoElement.setAttribute('fill', 'inherit');
-        icoElement.removeAttribute('width');
-        icoElement.removeAttribute('height');
+        this.absoluteStrokeWidth = this.absoluteStrokeWidth ?? false;
+        if (changes.name) {
+            const icoOfName = this.getIcon(this.toPascalCase(changes.name.currentValue));
 
-        this.elem.nativeElement.innerHTML = '';
-        this.elem.nativeElement.append(icoElement);
-      }
-    }
-    else if (changes.img) {
-      const icoElement = createElement(changes.img.currentValue);
-      icoElement.setAttribute('stroke-width', 'inherit');
-      icoElement.setAttribute('fill', 'inherit');
-      icoElement.removeAttribute('width');
-      icoElement.removeAttribute('height');
+            if (!icoOfName) {
+                console.warn(
+                    `Icon not found: ${changes.name.currentValue}\n` +
+                    "Please check icon name or 'lucide icon list'"
+                );
+            } else {
+                this.replaceElement(icoOfName);
+            }
+        } else if (changes.img) {
+            this.replaceElement(changes.img.currentValue);
+        }
 
-      this.elem.nativeElement.innerHTML = '';
-      this.elem.nativeElement.append(icoElement);
+        this.changeDetector.markForCheck();
     }
 
-    this.changeDetector.markForCheck();
-  }
+    replaceElement(img: LucideIconData): void {
+        const attributes = {
+            ...defaultAttributes,
+            ...img[1],
+            width:
+                typeof this.size === 'number'
+                    ? this.size.toString(10)
+                    : this.size,
+            height:
+                typeof this.size === 'number'
+                    ? this.size.toString(10)
+                    : this.size,
+            stroke: this.color,
+            'stroke-width': this.absoluteStrokeWidth
+                ? formatFixed(this._strokeWidth / (this._size / this.defaultSize))
+                : this.strokeWidth.toString(10),
+        };
+        const icoElement = this.createElement([img[0], attributes, img[2]]);
+        icoElement.classList.add('lucide');
+        if (this.name) {
+            icoElement.classList.add(`lucide-${this.name.replace('_', '-')}`);
+        }
+        if (this.class) {
+            icoElement.classList.add(...this.class.split(/ /).map((a) => a.trim()).filter((a) => a.length > 0))
+        }
+        const childElements = this.elem.nativeElement.childNodes;
+        for (let child of childElements) {
+            this.renderer.removeChild(this.elem.nativeElement, child);
+        }
+        this.renderer.appendChild(this.elem.nativeElement, icoElement);
+    }
 
-  toPascalCase(str: string): string {
-    return str.replace(/(\w)([a-z0-9]*)(_|-|\s*)/g, (g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase());
-  }
+    toPascalCase(str: string): string {
+        return str.replace(
+            /(\w)([a-z0-9]*)(_|-|\s*)/g,
+            (g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase()
+        );
+    }
+
+    private parseNumber(value: string | number): number {
+        if (typeof value === 'string') {
+            const parsedValue = parseInt(value, 10);
+            if (isNaN(parsedValue)) {
+                throw new Error(`${value} is not numeric.`);
+            }
+            return parsedValue;
+        }
+        return value;
+    }
+
+    private getIcon(name: string): LucideIconData | null {
+        for (const iconProvider of Array.isArray(this.iconProviders) ? this.iconProviders : [this.iconProviders]) {
+            if (iconProvider.hasIcon(name)) {
+                return iconProvider.getIcon(name);
+            }
+        }
+        return null;
+    }
+
+
+    private createElement([tag, attrs, children = []]: LucideIconData) {
+        const element = this.renderer.createElement(tag, 'http://www.w3.org/2000/svg');
+
+        Object.keys(attrs).forEach((name) => {
+            const attrValue: string =
+                typeof attrs[name] === 'string'
+                    ? (attrs[name] as string)
+                    : attrs[name].toString(10);
+            this.renderer.setAttribute(element, name, attrValue);
+        });
+
+        if (children.length) {
+            children.forEach((child: LucideIconData) => {
+                const childElement = this.createElement(child);
+                this.renderer.appendChild(element, childElement);
+            });
+        }
+
+        return element;
+    }
 }
