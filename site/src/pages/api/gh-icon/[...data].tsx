d3ad14e473d1a1a6f8033a38aaba2fca61614933
Jakob Guddas <github@jguddas.de>
2023-04-16 16:30:49 +0200

    feat: changed fill pattern

--- site/src/pages/api/gh-icon/[...data].tsx
+++ site/src/pages/api/gh-icon/[...data].tsx
@@ -33,17 +33,41 @@ export default async function handler(req, res) {
 
       backdrop = (
         <>
+          <defs xmlns="http://www.w3.org/2000/svg">
+            <pattern
+              id="pattern"
+              width=".1"
+              height=".1"
+              patternUnits="userSpaceOnUse"
+              patternTransform="rotate(45 50 50)"
+            >
+              <line stroke="#ff0000" stroke-width=".1" y2="1" />
+              <line stroke="#ff0000" stroke-width=".1" y2="1" />
+            </pattern>
+          </defs>
           <mask id="svg-preview-backdrop-mask-outline" maskUnits="userSpaceOnUse">
             <g stroke="#fff" dangerouslySetInnerHTML={{ __html: backdropString }} />
-            <g strokeWidth={2.25} dangerouslySetInnerHTML={{ __html: src }} />
+            <g dangerouslySetInnerHTML={{ __html: src }} />
           </mask>
           <mask id="svg-preview-backdrop-mask-fill" maskUnits="userSpaceOnUse">
             <g stroke="#fff" dangerouslySetInnerHTML={{ __html: backdropString }} />
-            <g strokeWidth={2.25} dangerouslySetInnerHTML={{ __html: src }} />
+            <g dangerouslySetInnerHTML={{ __html: src }} />
             <g strokeWidth={1.75} dangerouslySetInnerHTML={{ __html: backdropString }} />
           </mask>
-          <g strokeWidth={2.5} stroke="#B80F0A" mask={'url(#svg-preview-backdrop-mask-outline)'}>
-            <rect x="0" y="0" width="24" height="24" fill="red" opacity={0.5} stroke="none" />
+          <g
+            strokeWidth={2.25}
+            stroke="url(#pattern)"
+            mask={'url(#svg-preview-backdrop-mask-outline)'}
+          >
+            <rect
+              x="0"
+              y="0"
+              width="24"
+              height="24"
+              fill="url(#pattern)"
+              opacity={0.5}
+              stroke="none"
+            />
             <g dangerouslySetInnerHTML={{ __html: src }} />
           </g>
           <rect
@@ -51,7 +75,7 @@ export default async function handler(req, res) {
             y="0"
             width="24"
             height="24"
-            fill="#B80F0A"
+            fill="url(#pattern)"
             stroke="none"
             mask={'url(#svg-preview-backdrop-mask-fill)'}
           />
