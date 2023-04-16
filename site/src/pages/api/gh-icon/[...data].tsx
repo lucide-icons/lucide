12a667c8b891e79c947932a5b97b26417d66377a
Jakob Guddas <github@jguddas.de>
2023-04-16 11:54:21 +0200

    feat: added backdrop to gh-icon

--- site/src/pages/api/gh-icon/[...data].tsx
+++ site/src/pages/api/gh-icon/[...data].tsx
@@ -1,3 +1,4 @@
+import { getData } from 'src/lib/icons';
 import SvgPreview from '../../../components/SvgPreview';
 
 export default async function handler(req, res) {
@@ -5,12 +6,18 @@ export default async function handler(req, res) {
   // https://github.com/vercel/next.js/issues/43810
   const ReactDOMServer = (await import('react-dom/server')).default;
 
-  const url = req.url.split('/').at(-1);
-  const data = url.slice(0, -4);
+  const url = req.url.split('/');
+  let backdrop: string;
+  try {
+    if (url.at(-2) !== 'gh-icon') {
+      backdrop = (await getData(url.at(-2))).src;
+    }
+  } catch (e) {}
+  const data = url.at(-1).slice(0, -4);
   const src = Buffer.from(data, 'base64').toString('utf8');
 
   const svg = Buffer.from(
-    ReactDOMServer.renderToString(<SvgPreview src={src} showGrid />)
+    ReactDOMServer.renderToString(<SvgPreview src={src} showGrid backdrop={backdrop} />)
   );
 
   res.setHeader('Cache-Control', 'public,max-age=31536000');
