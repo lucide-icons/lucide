7b10f2f97bd71be9a4d5d178d72d763259ff8584
Karsa <karsa@karsa.org>
2023-04-27 14:14:22 +0200

    [lucide-angular] Export aliases + fix global config issues

--- packages/lucide-angular/src/public-api.ts
+++ packages/lucide-angular/src/public-api.ts
@@ -1,8 +1,10 @@
 import * as icons from './icons/lucide-icons';
+import * as aliases from './aliases';
 
 export * from './lib/lucide-angular.component';
 export * from './lib/lucide-angular.module';
 export * from './lib/lucide-icon.config';
 export * from './lib/lucide-icon.provider';
 export * from './icons/lucide-icons';
+export * from './aliases';
 export { icons };
