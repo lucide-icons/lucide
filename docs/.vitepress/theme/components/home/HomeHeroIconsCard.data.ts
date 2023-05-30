b760bcbe251727f28fc5c65920e9e66ef008e1a4
Eric Fennis <eric.fennis@gmail.com>
2023-05-30 11:11:51 +0200

    Fix import

--- docs/.vitepress/theme/components/home/HomeHeroIconsCard.data.ts
+++ docs/.vitepress/theme/components/home/HomeHeroIconsCard.data.ts
@@ -1,10 +1,10 @@
-import { getAllData } from '../../../lib/icons'
+import iconNodes from '../../../data/iconNodes'
 
 const getRandomItem = <Item>(items: Item[]): Item => items[Math.floor(Math.random()*items.length)];
 
 export default {
   async load() {
-    const icons = await getAllData()
+    const icons = Object.values(iconNodes)
 
     const randomIcons = Array.from({ length: 200 }, () => getRandomItem(icons))
 
