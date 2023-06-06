8c217618e5fb60a13886cc1af968dd2236284054
Eric Fennis <eric.fennis@gmail.com>
2023-06-06 17:05:04 +0200

    Add og image

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,11 +8,15 @@ import sidebar from './sidebar';
 
 const links = []
 
+
+const title = "Lucide Icons";
+const description = "Beautiful & consistent icon toolkit made by the community."
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  title,
+  description,
   cleanUrls: true,
-  title: "Lucide",
-  description: "Beautiful & consistent icon toolkit made by the community.",
   outDir: '.vercel/output/static',
   vite: {
     resolve: {
@@ -33,14 +37,67 @@ export default defineConfig({
     }
   },
   head: [
-    [
-      'script',
-      {
-        src: 'https://plausible.io/js/script.js',
-        'data-domain': 'lucide.dev',
-        defer: ''
-      }
-    ],
+    [ 'script', {
+      src: 'https://plausible.io/js/script.js',
+      'data-domain': 'lucide.dev',
+      defer: ''
+    }],
+    [ 'meta', {
+      property:"og:locale",
+      content:"en_US"
+    }],
+    [ 'meta', {
+      property:"og:type",
+      content:"website"
+    }],
+    [ 'meta', {
+      property:"og:site_name",
+      content: title,
+    }],
+    [ 'meta', {
+      property:"og:title",
+      content: title,
+    }],
+    [ 'meta', {
+      property:"og:description",
+      content: description
+    }],
+    [ 'meta', {
+      property:"og:url",
+      content:"https://lucide.dev"
+    }],
+    [ 'meta', {
+      property:"og:image",
+      content: "https://lucide.dev/og.png"
+    }],
+    [ 'meta', {
+      property:"og:image:width",
+      content:"1200"
+    }],
+    [ 'meta', {
+      property:"og:image:height",
+      content:"630"
+    }],
+    [ 'meta', {
+      property:"og:image:type",
+      content:"image/png"
+    }],
+    [ 'meta', {
+      property:"twitter:card",
+      content:"summary_large_image"
+    }],
+    [ 'meta', {
+      property:"twitter:title",
+      content: title,
+    }],
+    [ 'meta', {
+      property:"twitter:description",
+      content: description
+    }],
+    [ 'meta', {
+      property:"twitter:image",
+      content:"https://lucide.dev/og.png"
+    }],
   ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
