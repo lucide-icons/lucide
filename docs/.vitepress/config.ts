2814a63b8fc3ee799b38c12def80471db1f4e8d2
Eric Fennis <eric.fennis@gmail.com>
2023-06-07 08:25:46 +0200

    Add sitemap and OG image to site (#1347)
    * Add sitemap generation
    
    * Add og image
    
    * Fix links
    
    * remove comments

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 import path from 'path';
 import { defineConfig } from 'vitepress'
+import { createWriteStream } from 'node:fs'
+import { resolve } from 'node:path'
+import { SitemapStream } from 'sitemap'
 import sidebar from './sidebar';
+
+const links = []
+
+
+const title = "Lucide Icons";
+const description = "Beautiful & consistent icon toolkit made by the community."
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  title,
+  description,
   cleanUrls: true,
-  title: "Lucide",
-  description: "Beautiful & consistent icon toolkit made by the community.",
   outDir: '.vercel/output/static',
   vite: {
     resolve: {
@@ -28,14 +37,67 @@ export default defineConfig({
     }
   },
   head: [
-    [
-      'script',
-      {
-        src: 'https://plausible.io/js/script.js',
-        'data-domain': 'lucide.dev',
-        defer: ''
-      }
-    ],
+    [ 'script', {
+      src: 'https://plausible.io/js/script.js',
+      'data-domain': 'lucide.dev',
+      defer: ''
+    }],
+    [ 'meta', {
+      property:"og:locale",
+      content:"en_US"
+    }],
+    [ 'meta', {
+      property:"og:type",
+      content:"website"
+    }],
+    [ 'meta', {
+      property:"og:site_name",
+      content: title,
+    }],
+    [ 'meta', {
+      property:"og:title",
+      content: title,
+    }],
+    [ 'meta', {
+      property:"og:description",
+      content: description
+    }],
+    [ 'meta', {
+      property:"og:url",
+      content:"https://lucide.dev"
+    }],
+    [ 'meta', {
+      property:"og:image",
+      content: "https://lucide.dev/og.png"
+    }],
+    [ 'meta', {
+      property:"og:image:width",
+      content:"1200"
+    }],
+    [ 'meta', {
+      property:"og:image:height",
+      content:"630"
+    }],
+    [ 'meta', {
+      property:"og:image:type",
+      content:"image/png"
+    }],
+    [ 'meta', {
+      property:"twitter:card",
+      content:"summary_large_image"
+    }],
+    [ 'meta', {
+      property:"twitter:title",
+      content: title,
+    }],
+    [ 'meta', {
+      property:"twitter:description",
+      content: description
+    }],
+    [ 'meta', {
+      property:"twitter:image",
+      content:"https://lucide.dev/og.png"
+    }],
   ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
@@ -44,7 +106,7 @@ export default defineConfig({
       dark: '/logo.dark.svg'
     },
     nav: [
-      { text: 'Icons', link: '/icons/index.md' },
+      { text: 'Icons', link: '/icons/' },
       { text: 'Guide', link: '/guide/' },
       { text: 'Packages', link: '/packages' },
       { text: 'License', link: '/license' },
@@ -61,5 +123,37 @@ export default defineConfig({
     editLink: {
       pattern: 'https://github.com/lucide-icons/lucide/edit/main/docs/:path'
     },
-  }
+  },
+  transformHtml: (_, id, { pageData }) => {
+    if (/[\\/]404\.html$/.test(id)) {
+      return
+    }
+
+    if (pageData.relativePath === 'index.md') {
+      console.log('Home!');
+    }
+
+    if (pageData.relativePath.startsWith('icons/')) {
+      links.push({
+        url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
+        lastmod: pageData?.params?.changedRelease?.date
+      })
+      return
+    }
+
+    links.push({
+      url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
+      lastmod: pageData.lastUpdated
+    })
+  },
+  buildEnd: async ({ outDir }) => {
+    const sitemap = new SitemapStream({
+      hostname: 'https://lucide.dev/'
+    })
+    const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
+    sitemap.pipe(writeStream)
+    links.forEach((link) => sitemap.write(link))
+    sitemap.end()
+    await new Promise((r) => writeStream.on('finish', r))
+  },
 })
