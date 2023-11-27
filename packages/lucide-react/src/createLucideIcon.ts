e022c9577c4eca21bfdefacdc955267d535787fa
Sukomal07 <hypersukomal@gmail.com>
2023-11-27 20:44:38 +0530

    remove extra space in svg class attribute

--- packages/lucide-react/src/createLucideIcon.ts
+++ packages/lucide-react/src/createLucideIcon.ts
@@ -1,14 +1,21 @@
-import { forwardRef, createElement, ReactSVG, SVGProps, ForwardRefExoticComponent, RefAttributes } from 'react';
+import {
+  forwardRef,
+  createElement,
+  ReactSVG,
+  SVGProps,
+  ForwardRefExoticComponent,
+  RefAttributes,
+} from 'react';
 import defaultAttributes from './defaultAttributes';
 
-export type IconNode = [elementName: keyof ReactSVG, attrs: Record<string, string>][]
+export type IconNode = [elementName: keyof ReactSVG, attrs: Record<string, string>][];
 
-export type SVGAttributes = Partial<SVGProps<SVGSVGElement>>
-type ComponentAttributes = RefAttributes<SVGSVGElement> & SVGAttributes
+export type SVGAttributes = Partial<SVGProps<SVGSVGElement>>;
+type ComponentAttributes = RefAttributes<SVGSVGElement> & SVGAttributes;
 
 export interface LucideProps extends ComponentAttributes {
-  size?: string | number
-  absoluteStrokeWidth?: boolean
+  size?: string | number;
+  absoluteStrokeWidth?: boolean;
 }
 
 export type LucideIcon = ForwardRefExoticComponent<LucideProps>;
@@ -20,11 +27,25 @@ export type LucideIcon = ForwardRefExoticComponent<LucideProps>;
  * @param {string} string
  * @returns {string} A kebabized string
  */
-export const toKebabCase = (string: string) => string.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
+export const toKebabCase = (string: string) =>
+  string
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .toLowerCase()
+    .trim();
 
 const createLucideIcon = (iconName: string, iconNode: IconNode): LucideIcon => {
   const Component = forwardRef<SVGSVGElement, LucideProps>(
-    ({ color = 'currentColor', size = 24, strokeWidth = 2, absoluteStrokeWidth, children, ...rest }, ref) =>
+    (
+      {
+        color = 'currentColor',
+        size = 24,
+        strokeWidth = 2,
+        absoluteStrokeWidth,
+        children,
+        ...rest
+      },
+      ref
+    ) =>
       createElement(
         'svg',
         {
@@ -33,17 +54,17 @@ const createLucideIcon = (iconName: string, iconNode: IconNode): LucideIcon => {
           width: size,
           height: size,
           stroke: color,
-          strokeWidth: absoluteStrokeWidth ? Number(strokeWidth) * 24 / Number(size) : strokeWidth,
+          strokeWidth: absoluteStrokeWidth
+            ? (Number(strokeWidth) * 24) / Number(size)
+            : strokeWidth,
           className: `lucide lucide-${toKebabCase(iconName)}`,
           ...rest,
         },
         [
           ...iconNode.map(([tag, attrs]) => createElement(tag, attrs)),
-          ...(
-            (Array.isArray(children) ? children : [children]) || []
-          )
-        ],
-      ),
+          ...((Array.isArray(children) ? children : [children]) || []),
+        ]
+      )
   );
 
   Component.displayName = `${iconName}`;
@@ -51,4 +72,4 @@ const createLucideIcon = (iconName: string, iconNode: IconNode): LucideIcon => {
   return Component;
 };
 
-export default createLucideIcon
+export default createLucideIcon;
