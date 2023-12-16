e7ad67790ae5f881871a844e3a3ec8d957760854
Eric Fennis <eric.fennis@gmail.com>
2023-12-16 20:30:55 +0100

    fix preview script

--- scripts/getIconPreviewMarkup.mjs
+++ scripts/getIconPreviewMarkup.mjs
@@ -14,57 +14,87 @@ const changedFiles = changedFilesPathString
   .map((file) => file.replace('.json', '.svg'))
   .filter((file, idx, arr) => arr.indexOf(file) === idx);
 
-const getImageTagsByFiles = (files, url) =>
+const getImageTagsByFiles = (files, getBaseUrl, width) =>
   files
     .map((file) => {
-      let svg = fs.readFileSync(path.join(process.cwd(), file), 'utf-8');
-      svg = svg.replace(/<svg[^>]*>/, '<svg>').replaceAll(/\n| {2}|\t/g, '');
+      const svgContent = fs.readFileSync(path.join(process.cwd(), file), 'utf-8');
+      const strippedAttrsSVG = svgContent
+        .replace(/<svg[^>]*>/, '<svg>')
+        .replaceAll(/\n| {2}|\t/g, '');
 
-      const base64 = Buffer.from(svg).toString('base64');
+      const base64 = Buffer.from(strippedAttrsSVG).toString('base64');
+      const url = getBaseUrl(file);
+      const widthAttr = width ? `width="${width}"` : '';
 
-      return `<img title="${file}" alt="${file}" src="${url}/${base64}.svg"/>`;
+      return `<img title="${file}" alt="${file}" ${widthAttr} src="${url}/${base64}.svg"/>`;
     })
-    .join('\n');
+    .join('');
 
-const svgFiles = readSvgDirectory(ICONS_DIR);
+const svgFiles = readSvgDirectory(ICONS_DIR).map((file) => `icons/${file}`);
 
 const iconsFilteredByName = (search) => svgFiles.filter((file) => file.includes(search));
 
-const changeFilesImageTags = getImageTagsByFiles(
-  changedFiles,
-  'https://lucide.dev/api/gh-icon/stroke-width/2',
-);
 const cohesionRandomImageTags = getImageTagsByFiles(
-  shuffle(svgFiles).slice(0, 10),
-  'https://lucide.dev/api/gh-icon/stroke-width/2',
-);
-const cohesionSquaresImageTags = getImageTagsByFiles(
-  iconsFilteredByName('square'),
-  'https://lucide.dev/api/gh-icon/stroke-width/2',
+  shuffle(svgFiles).slice(0, changedFiles.length),
+  () => 'https://lucide.dev/api/gh-icon/stroke-width/2',
 );
 
-console.log(changeFilesImageTags);
-// console.log(`\
-// ### Added or changed icons
-// ${ changeFilesImageTags }<br/>
-// <details>
-// <summary>Preview cohesion</summary>
-// ${ steps.generate-cohesion-check-squares.outputs.body }<br/>
-// ${ steps.generate-2px-stroke-width.outputs.body }<br/>
-// ${ steps.generate-cohesion-check-random.outputs.body }<br/>
-// </details>
-// <details>
-// <summary>Preview stroke widths</summary>
-// ${ steps.generate-1px-stroke-width.outputs.body }<br/>
-// ${ steps.generate-2px-stroke-width.outputs.body }<br/>
-// ${ steps.generate-3px-stroke-width.outputs.body }<br/>
-// </details>
-// <details>
-// <summary>DPI Preview (24px)</summary>
-// ${ steps.generate-24px-dpi-preview.outputs.body }<br/>
-// </details>
-// <details>
-// <summary>Icon X-rays</summary>
-// ${ steps.generate-x-rays.outputs.body }
-// </details>
-// `)
+const cohesionSquaresImageTags = getImageTagsByFiles(
+  shuffle(iconsFilteredByName('square')).slice(0, changedFiles.length),
+  () => 'https://lucide.dev/api/gh-icon/stroke-width/2',
+);
+
+const changeFiles1pxStrokeImageTags = getImageTagsByFiles(
+  changedFiles,
+  () => 'https://lucide.dev/api/gh-icon/stroke-width/1',
+);
+
+const changeFiles2pxStrokeImageTags = getImageTagsByFiles(
+  changedFiles,
+  () => 'https://lucide.dev/api/gh-icon/stroke-width/2',
+);
+
+const changeFiles3pxStrokeImageTags = getImageTagsByFiles(
+  changedFiles,
+  () => 'https://lucide.dev/api/gh-icon/stroke-width/3',
+);
+
+const changeFilesLowDPIImageTags = getImageTagsByFiles(
+  changedFiles,
+  () => 'https://lucide.dev/api/gh-icon/dpi/24',
+);
+
+const changeFilesXRayImageTags = getImageTagsByFiles(
+  changedFiles,
+  (file) => {
+    const iconName = path.basename(file, '.svg');
+
+    return `https://lucide.dev/api/gh-icon/${iconName}`;
+  },
+  400,
+);
+
+console.log(`\
+### Added or changed icons
+${changeFiles2pxStrokeImageTags}
+<details>
+<summary>Preview cohesion</summary>
+${cohesionSquaresImageTags}<br/>
+${changeFiles2pxStrokeImageTags}<br/>
+${cohesionRandomImageTags}<br/>
+</details>
+<details>
+<summary>Preview stroke widths</summary>
+${changeFiles1pxStrokeImageTags}<br/>
+${changeFiles2pxStrokeImageTags}<br/>
+${changeFiles3pxStrokeImageTags}<br/>
+</details>
+<details>
+<summary>DPI Preview (24px)</summary>
+${changeFilesLowDPIImageTags}<br/>
+</details>
+<details>
+<summary>Icon X-rays</summary>
+${changeFilesXRayImageTags}
+</details>
+`);
