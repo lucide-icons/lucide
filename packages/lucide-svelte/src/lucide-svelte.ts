5a8182c8e8cb270e0c10db8cff2e7434e5b901af
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-28 00:14:42 -0500

    export Icon type

--- packages/lucide-svelte/src/lucide-svelte.ts
+++ packages/lucide-svelte/src/lucide-svelte.ts
@@ -2,3 +2,4 @@ export * from './icons/index.js';
 export * as icons from './icons/index.js';
 export * from './aliases.js';
 export { default as defaultAttributes } from './defaultAttributes.js';
+export type { Icon } from './types.js';
