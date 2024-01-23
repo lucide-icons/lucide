9e68779d22254c12863cf82eea9d8f151472ce2a
Eric Fennis <eric.fennis@gmail.com>
2024-01-23 17:45:43 +0100

    create new vue directory

---
+++ packages/vue/rollup.config.mjs
@@ -0,0 +1,75 @@
+import plugins, { replace } from '@lucide/rollup-plugins';
+import pkg from './package.json' assert { type: 'json' };
+import dts from "rollup-plugin-dts";
+
+const packageName = 'LucideVue';
+const outputFileName = 'lucide-vue';
+const outputDir = 'dist';
+const inputs = ['src/lucide-vue.ts'];
+const bundles = [
+  {
+    format: 'umd',
+    inputs,
+    outputDir,
+    minify: true,
+  },
+  {
+    format: 'umd',
+    inputs,
+    outputDir,
+  },
+  {
+    format: 'cjs',
+    inputs,
+    outputDir,
+  },
+  {
+    format: 'esm',
+    inputs,
+    outputDir,
+    preserveModules: true,
+  },
+];
+
+const configs = bundles
+  .map(({ inputs, outputDir, format, minify, preserveModules }) =>
+    inputs.map(input => ({
+      input,
+      plugins: plugins(pkg, minify),
+      external: ['vue'],
+      output: {
+        name: packageName,
+        ...(preserveModules
+          ? {
+              dir: `${outputDir}/${format}`,
+            }
+          : {
+              file: `${outputDir}/${format}/${outputFileName}${minify ? '.min' : ''}.js`,
+            }),
+        format,
+        preserveModules,
+        sourcemap: true,
+        globals: {
+          vue: 'vue',
+        },
+      },
+    })),
+  )
+  .flat();
+
+  export default [
+    {
+      input: inputs[0],
+      output: [{
+        file: `dist/${outputFileName}.d.ts`, format: "es"
+      }],
+      plugins: [
+        dts({
+          compilerOptions: {
+            preserveSymlinks: false
+          }
+        })
+      ],
+    },
+    ...configs
+  ];
