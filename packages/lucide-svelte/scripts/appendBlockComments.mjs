54c8d4078d9acf67f5d0f20418c1c717d817a699
CokaKoala <31664583+AdrianGonz97@users.noreply.github.com>
2024-01-29 13:48:14 -0500

    fix: Add the license banner inside of the script tag instead of an HTML comment (#1811)
    * add banner inside of the script tag instead
    
    * renamed script
    
    * Update packages/lucide-svelte/scripts/license.mjs
    
    Co-authored-by: Jakob Guddas <github@jguddas.de>
    
    * Update packages/lucide-svelte/scripts/appendLicense.mjs
    
    Co-authored-by: Eric Fennis <eric.fennis@gmail.com>
    
    * renamed file
    
    ---------
    
    Co-authored-by: Jakob Guddas <github@jguddas.de>
    Co-authored-by: Eric Fennis <eric.fennis@gmail.com>

--- packages/lucide-svelte/scripts/appendBlockComments.mjs
+++ packages/lucide-svelte/scripts/appendBlockComments.mjs
@@ -1,13 +1,8 @@
-import { getCurrentDirPath } from '../../../scripts/helpers.mjs';
 import { lstatSync } from 'fs';
 import { readdir, readFile, writeFile } from 'fs/promises';
 import path from 'path';
-import pkg from '../package.json' assert { type: 'json' };
-
-const BANNER = `@license ${pkg.name} v${pkg.version} - ${pkg.license}
-
-This source code is licensed under the ${pkg.license} license.
-See the LICENSE file in the root directory of this source tree.`;
+import { getCurrentDirPath } from '../../../scripts/helpers.mjs';
+import { getJSBanner } from './license.mjs';
 
 const currentDir = getCurrentDirPath(import.meta.url);
 const targetDirectory = path.join(currentDir, '../dist');
@@ -32,21 +27,16 @@ for (const file of files) {
     license = getJSBanner();
   }
 
-  if (/\.svelte/.test(ext)) {
-    license = getSvelteBanner();
-  }
-
   if (license) {
     newContents = license + contents;
-
   }
 
   // Places icon block comment at the top of the Svelte component class
-  if(/icons\/(.*?)\.svelte\.d\.ts/.test(filepath)) {
-    const svelteFilepath = filepath.replace('.d.ts', '')
+  if (/icons\/(.*?)\.svelte\.d\.ts/.test(filepath)) {
+    const svelteFilepath = filepath.replace('.d.ts', '');
     let svelteFileContents = await readFile(svelteFilepath, { encoding: 'utf-8' });
 
-    const blockCommentRegex = /\/\*\*[\s\S]*?\*\//;
+    const blockCommentRegex = /\/\*\*\n\s\*\s(@component\s@name)[\s\S]*?\*\//;
     const blockCommentMatch = blockCommentRegex.exec(svelteFileContents);
 
     if (blockCommentMatch !== null) {
@@ -55,26 +45,15 @@ for (const file of files) {
       const exportClassRegex = /export default class (\w+) extends SvelteComponentTyped<(.*?)> {/;
 
       if (exportClassRegex.test(newContents)) {
-        newContents = newContents.replace(exportClassRegex, `${blockComment}\nexport default class $1 extends SvelteComponentTyped<$2> {`);
+        newContents = newContents.replace(
+          exportClassRegex,
+          `${blockComment}\nexport default class $1 extends SvelteComponentTyped<$2> {`
+        );
       }
     }
   }
 
-  if(newContents !== contents) {
+  if (newContents !== contents) {
     await writeFile(filepath, newContents, { encoding: 'utf-8' });
   }
-}
-
-function getJSBanner() {
-  return `/**
- * ${BANNER}
- */
-\n`;
-}
-
-function getSvelteBanner() {
-  return `<!--
-  ${BANNER}
--->
-\n`;
 }
