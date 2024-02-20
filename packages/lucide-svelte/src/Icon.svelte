acfb9226f4d08e20868ae7a8a420877eb26046c2
lolcabanon <90099349+lolcabanon@users.noreply.github.com>
2024-02-20 20:17:29 +0100

    Update Icon.svelte to use the exported context keys

--- packages/lucide-svelte/src/Icon.svelte
+++ packages/lucide-svelte/src/Icon.svelte
@@ -4,11 +4,18 @@
   import defaultAttributes from './defaultAttributes'
   import type { IconNode } from './types';
 
+  import {
+    LucideContextIconColor,
+    LucideContextIconSize,
+    LucideContextIconStrokeWidth,
+    LucideContextIconAbsoluteStrokeWidth
+  } from './contextKeys';
+
   export let name: string
-  export let color = getContext('lucide-icon-color') ?? 'currentColor'
-  export let size: number | string = getContext('lucide-icon-size') ?? 24
-  export let strokeWidth: number | string = getContext('lucide-icon-stroke-width') ?? 2
-  export let absoluteStrokeWidth: boolean = getContext('lucide-icon-absolute-stroke-width') ?? false
+  export let color = getContext(LucideContextIconColor) ?? 'currentColor'
+  export let size: number | string = getContext(LucideContextIconSize) ?? 24
+  export let strokeWidth: number | string = getContext(LucideContextIconStrokeWidth) ?? 2
+  export let absoluteStrokeWidth: boolean = getContext(LucideContextIconAbsoluteStrokeWidth) ?? false
   export let iconNode: IconNode
 </script>
 
