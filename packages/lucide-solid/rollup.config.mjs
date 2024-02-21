84967b1583d473140b066b52dd7176eb50ef844d
Eric Fennis <eric.fennis@gmail.com>
2024-02-21 21:11:35 +0100

    Transpile solid with esbuild

--- packages/lucide-solid/rollup.config.mjs
+++ packages/lucide-solid/rollup.config.mjs
@@ -1,20 +1,103 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import withSolid from 'rollup-preset-solid';
-import bundleSize from '@atomico/rollup-plugin-sizes';
-import license from 'rollup-plugin-license';
-
+import plugins, { preserveJSX } from '@lucide/rollup-plugins';
+import dts from 'rollup-plugin-dts';
+import { babel } from "@rollup/plugin-babel";
+import ts from 'typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import esbuild from 'esbuild';
+import fs from 'fs';
+import path from 'path';
 import pkg from './package.json' assert { type: 'json' };
 
-const config = withSolid({
-  targets: ['esm', 'cjs'],
-});
+const packageName = 'LucideSolid';
+const outputFileName = 'lucide-solid';
+const outputDir = 'dist';
+const inputs = ['src/lucide-solid.ts'];
 
-config.plugins = [
-  ...config.plugins,
-  license({
-    banner: `${pkg.name} v${pkg.version} - ${pkg.license}`,
-  }),
-  bundleSize(),
+const bundles = [
+  {
+    format: 'cjs',
+    inputs,
+    outputDir,
+  },
+  {
+    format: 'esm',
+    inputs,
+    outputDir,
+  },
 ];
 
-export default config;
+const configs = bundles
+  .map(({ inputs, outputDir, format, preserveModules }) =>
+    inputs.map((input) => ({
+      input,
+      plugins: [
+        babel({
+          extensions: [".ts", ".tsx", ".js"],
+          babelHelpers: "bundled",
+          presets: [
+            "babel-preset-solid",
+            "@babel/preset-typescript",
+            ["@babel/preset-env", { bugfixes: true, targets: 'last 2 years' }],
+          ],
+        }),
+        resolve({ extensions: ['.js', '.ts', '.jsx' ]}),
+        ...plugins({
+          pkg,
+          withEsbuild: false,
+        }),
+        (format === 'esm' ? {
+          name: "ts",
+          buildEnd() {
+            // Transpile typescript to './dist/source'
+            esbuild.build({
+              entryPoints: ['./src/**.tsx', './src/**.ts'],
+              outdir: './dist/source',
+              loader: {
+                '.js': 'jsx',
+              },
+              jsx: 'preserve',
+              bundle: true,
+              format: 'esm',
+              sourcemap: true,
+              target: ['esnext'],
+              external: ['solid-js'],
+            });
+
+            // Generate types
+            const program = ts.createProgram([pkg.source], {
+              target: ts.ScriptTarget.ESNext,
+              module: ts.ModuleKind.ESNext,
+              moduleResolution: ts.ModuleResolutionKind.NodeJs,
+              jsx: ts.JsxEmit.Preserve,
+              jsxImportSource: "solid-js",
+              allowSyntheticDefaultImports: true,
+              esModuleInterop: true,
+              declarationDir: `dist/types`,
+              declaration: true,
+              emitDeclarationOnly: true,
+            });
+            program.emit();
+          }
+        } : null),
+      ],
+      external: ['solid-js', 'solid-js/web', 'solid-js/store'],
+      output: {
+        name: packageName,
+        ...(preserveModules
+          ? {
+              dir: `${outputDir}/${format}`,
+              exports: 'auto',
+            }
+          : {
+              file: `${outputDir}/${format}/${outputFileName}.js`,
+            }),
+        format: format === 'source' ? 'esm' : format,
+        preserveModules,
+        preserveModulesRoot: 'src',
+        sourcemap: true,
+      },
+    })),
+  )
+  .flat();
+
+export default configs;
