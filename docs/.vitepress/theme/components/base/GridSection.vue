8b2dd1a67f82509c483a3171e81cdfce7ecd2798
Eric Fennis <eric.fennis@gmail.com>
2024-02-29 14:27:58 +0100

    Add team member cards

--- docs/.vitepress/theme/components/base/GridSection.vue
+++ docs/.vitepress/theme/components/base/GridSection.vue
@@ -30,34 +30,4 @@ const headingElement = computed(() => `h${props.headingLevel}`)
 section {
   margin-bottom: 96px;
 }
-
-.grid {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: stretch;
-  justify-content: center;
-  align-content: space-evenly;
-  box-sizing: border-box;
-  margin: -8px;
-}
-
-.grid > * {
-  flex-basis: 100%;
-  box-sizing: border-box;
-  padding: 8px;
-}
-
-@media (min-width: 960px) {
-  .grid > * {
-    flex-basis: 50%;
-  }
-}
-
-@media (min-width: 1280px) {
-  .grid > * {
-    flex-basis: 33.33%;
-  }
-}
-
-
 </style>
---
+++ docs/.vitepress/theme/components/home/HomeTeamSection.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import HomeContainer from './HomeContainer.vue'
+import GridSection from '../base/GridSection.vue'
+import TeamMemberCard, { TeamMember } from './TeamMemberCard.vue'
+import teamData from '../../../data/teamData.json'
+</script>
+
+<template>
+   <HomeContainer>
+    <GridSection
+      title="Meet the team"
+      :headingLevel="2"
+      class="team-members"
+    >
+      <TeamMemberCard
+        v-for="teamMember in (teamData as TeamMember[])"
+        v-bind="teamMember"
+      />
+    </GridSection>
+   </HomeContainer>
+</template>
+
+
+<style scoped>
+.team-members {
+  gap: 24px;
+  margin-top: 48px;
+  margin-bottom: 48px;
+}
+
+.team-members :deep(.card-grid) {
+
+}
+
+@media (min-width: 640px) {
+  .team-members :deep(.card-grid > *) {
+    flex-basis: 50%;
+  }
+}
+
+@media (min-width: 768px) {
+  .team-members :deep(.card-grid > *) {
+    flex-basis: 33.33%;
+  }
+}
+</style>
