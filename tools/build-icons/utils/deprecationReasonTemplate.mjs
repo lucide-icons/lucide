353faf4ddd3bd53966587afb97c271bcb968f470
Karsa <karsa@sztaki.hu>
2024-03-11 08:18:55 +0100

    fix(build): deprecate aliases using numbers

--- tools/build-icons/utils/deprecationReasonTemplate.mjs
+++ tools/build-icons/utils/deprecationReasonTemplate.mjs
@@ -5,6 +5,8 @@ export function deprecationReasonTemplate(
   switch (deprecationReason) {
     case 'alias.typo':
       return `Renamed because of typo, use {@link ${componentName}} instead. This alias will be removed in ${toBeRemovedInVersion}`;
+    case 'alias.naming':
+      return `This name is no longer compliant with our guidelines, use {@link ${componentName}} instead. This alias will be removed in ${toBeRemovedInVersion}`;
     case 'icon.brand':
       return `Brand icons have been deprecated and are due to be removed, please refer to https://github.com/lucide-icons/lucide/issues/670. We recommend using https://simpleicons.org/?q=${iconName} instead. This icon will be removed in ${toBeRemovedInVersion}`;
   }
