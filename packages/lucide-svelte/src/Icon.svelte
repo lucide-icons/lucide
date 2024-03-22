34e647d8f1a6e0a03a29c81a8572e091cc182a36
Eric Fennis <eric.fennis@gmail.com>
2024-03-22 14:31:55 +0100

    Enable Svelte component

--- packages/lucide-svelte/src/Icon.svelte
+++ packages/lucide-svelte/src/Icon.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+  import { mergeClasses } from '@lucide/shared'
   import defaultAttributes from './defaultAttributes'
   import type { IconNode } from './types';
 
-  export let name: string
+  export let name: string | undefined
   export let color = 'currentColor'
   export let size: number | string = 24
   export let strokeWidth: number | string = 2
@@ -21,7 +22,14 @@
       ? Number(strokeWidth) * 24 / Number(size)
       : strokeWidth
   }
-  class={`lucide-icon lucide lucide-${name} ${$$props.class ?? ''}`}
+  class={
+    mergeClasses(
+      'lucide-icon',
+      'lucide',
+      name ? `lucide-${name}`: '',
+      $$props.class
+    )
+  }
 >
   {#each iconNode as [tag, attrs]}
     <svelte:element this={tag} {...attrs}/>
