4ae0d4b97618db8067c0bb1bd05a7e17475f48bf
Eric Fennis <eric.fennis@gmail.com>
2024-04-01 12:24:03 +0200

    Update package lock

--- packages/lucide-react-native/src/lucide-react-native.ts
+++ packages/lucide-react-native/src/lucide-react-native.ts
@@ -1,7 +1,7 @@
 export * from './icons';
 export * as icons from './icons';
 export * from './aliases';
-export * from './types'
+export * from './types';
 
 export { default as createLucideIcon } from './createLucideIcon';
 export { default as useIconComponent } from './useIconComponent';
