ac36c43747500924d6e8fa60b41d46331ee37e69
Eric Fennis <eric.fennis@gmail.com>
2024-04-26 17:59:04 +0200

    feat(icon-component): Creating icons with iconNodes (#1997)
    * Add useIconComponent, lucide-react
    
    * Add concept useIconComponent
    
    * add useIconComponents to packages
    
    * Add icon component
    
    * Add icon component
    
    * Add tests for react packages
    
    * Reset changes in icons
    
    * Add types
    
    * Add support for Icon components in Lucide Vue Next
    
    * update tests
    
    * Update tests
    
    * Enable Svelte component
    
    * Fix lucide-react-native tests
    
    * Update Solid package
    
    * update snapshots
    
    * Add docs
    
    * add docs
    
    * Update tests
    
    * Formatting
    
    * Formatting
    
    * Update package lock
    
    * Remove `useIconComponent`
    
    * Update guides
    
    * Update exports preact and solid package
    
    * Formatting
    
    * Format createIcons.ts
    
    * Add lucide lab repo link in docs

---
+++ packages/lucide-preact/src/Icon.ts
@@ -0,0 +1,50 @@
+import { h, toChildArray } from 'preact';
+import defaultAttributes from './defaultAttributes';
+import type { IconNode, LucideProps } from './types';
+
+interface IconComponentProps extends LucideProps {
+  iconNode: IconNode;
+}
+
+/**
+ * Lucide icon component
+ *
+ * @component Icon
+ * @param {object} props
+ * @param {string} props.color - The color of the icon
+ * @param {number} props.size - The size of the icon
+ * @param {number} props.strokeWidth - The stroke width of the icon
+ * @param {boolean} props.absoluteStrokeWidth - Whether to use absolute stroke width
+ * @param {string} props.class - The class name of the icon
+ * @param {IconNode} props.children - The children of the icon
+ * @param {IconNode} props.iconNode - The icon node of the icon
+ *
+ * @returns {ForwardRefExoticComponent} LucideIcon
+ */
+const Icon = ({
+  color = 'currentColor',
+  size = 24,
+  strokeWidth = 2,
+  absoluteStrokeWidth,
+  children,
+  iconNode,
+  class: classes = '',
+  ...rest
+}: IconComponentProps) =>
+  h(
+    'svg',
+    {
+      ...defaultAttributes,
+      width: String(size),
+      height: size,
+      stroke: color,
+      ['stroke-width' as 'strokeWidth']: absoluteStrokeWidth
+        ? (Number(strokeWidth) * 24) / Number(size)
+        : strokeWidth,
+      class: ['lucide', classes].join(' '),
+      ...rest,
+    },
+    [...iconNode.map(([tag, attrs]) => h(tag, attrs)), ...toChildArray(children)],
+  );
+
+export default Icon;
