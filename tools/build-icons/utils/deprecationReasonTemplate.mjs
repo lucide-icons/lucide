5a025ade2ee00ca50f7d120a6f595f13d78c75ea
Karsa <karsa@sztaki.hu>
2024-05-13 09:39:11 +0200

    feat(icon-schema): separate deprecation reason enumerations, extract removal notice

--- tools/build-icons/utils/deprecationReasonTemplate.mjs
+++ tools/build-icons/utils/deprecationReasonTemplate.mjs
@@ -2,12 +2,14 @@ export function deprecationReasonTemplate(
   deprecationReason,
   { componentName, iconName, toBeRemovedInVersion },
 ) {
+  const removalNotice = toBeRemovedInVersion ? ` This ${deprecationReason.startsWith('icon') ? 'icon' : 'alias'} will be removed in ${toBeRemovedInVersion}` : undefined
+
   switch (deprecationReason) {
     case 'alias.typo':
-      return `Renamed because of typo, use {@link ${componentName}} instead. This alias will be removed in ${toBeRemovedInVersion}`;
+      return `Renamed because of typo, use {@link ${componentName}} instead.${removalNotice}`;
     case 'alias.naming':
-      return `This name is no longer compliant with our guidelines, use {@link ${componentName}} instead. This alias will be removed in ${toBeRemovedInVersion}`;
+      return `This name is no longer compliant with our guidelines, use {@link ${componentName}} instead.${removalNotice}`;
     case 'icon.brand':
-      return `Brand icons have been deprecated and are due to be removed, please refer to https://github.com/lucide-icons/lucide/issues/670. We recommend using https://simpleicons.org/?q=${iconName} instead. This icon will be removed in ${toBeRemovedInVersion}`;
+      return `Brand icons have been deprecated and are due to be removed, please refer to https://github.com/lucide-icons/lucide/issues/670. We recommend using https://simpleicons.org/?q=${iconName} instead.${removalNotice}`;
   }
 }
