bc4f1d46b091bc383f2e495d71d3f05bcae24117
Jakob Guddas <github@jguddas.de>
2024-05-18 16:31:57 +0200

    feat(react): added aria-hidden fallback for decorative icons

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -35,3 +35,17 @@ export const mergeClasses = <ClassType = string | undefined | null>(...classes: 
       return Boolean(className) && array.indexOf(className) === index;
     })
     .join(' ');
+
+/**
+ * Check if a component has an accessibility prop
+ *
+ * @param {object} props
+ * @returns {boolean} Whether the component has an accessibility prop
+ */
+export const hasA11yProp = (props: Record<string, any>) => {
+  for (const prop in props) {
+    if (prop.startsWith('aria-') || prop === 'role' || prop === 'title') {
+      return true;
+    }
+  }
+};
