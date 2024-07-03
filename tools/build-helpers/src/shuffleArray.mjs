c35d8e0a4b1f934928e3105beca23e715499dcd4
Karsa <contact@karsa.org>
2024-07-03 21:56:14 +0200

    fix(tools): rename shuffle => shuffleArray

--- tools/build-helpers/src/shuffleArray.mjs
+++ tools/build-helpers/src/shuffleArray.mjs
@@ -3,7 +3,7 @@
  * @param {array} array
  * @returns {array}
  */
-export const shuffle = (array) => {
+export const shuffleArray = (array) => {
   // eslint-disable-next-line no-plusplus
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
