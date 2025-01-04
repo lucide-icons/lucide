fa5167120d85393279b0294e5de3b3206c9a1e94
ケイラ <mckayla@hey.com>
2025-01-03 22:27:18 -0700

    🧹

--- packages/lucide-react-native/src/types.ts
+++ packages/lucide-react-native/src/types.ts
@@ -1,5 +1,5 @@
-import type { ForwardRefExoticComponent } from 'react';
-import type { SvgProps } from 'react-native-svg';
+import type { ForwardRefExoticComponent } from "react";
+import type { SvgProps } from "react-native-svg";
 
 /**
  * Taken from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/776263e47903cf03cf64cfcc692defefcc5513ab/types/react/index.d.ts#L3873
@@ -7,68 +7,21 @@ import type { SvgProps } from 'react-native-svg';
  * support usage with older versions.
  */
 type SVGElementType =
-  | 'animate'
-  | 'circle'
-  | 'clipPath'
-  | 'defs'
-  | 'desc'
-  | 'ellipse'
-  | 'feBlend'
-  | 'feColorMatrix'
-  | 'feComponentTransfer'
-  | 'feComposite'
-  | 'feConvolveMatrix'
-  | 'feDiffuseLighting'
-  | 'feDisplacementMap'
-  | 'feDistantLight'
-  | 'feDropShadow'
-  | 'feFlood'
-  | 'feFuncA'
-  | 'feFuncB'
-  | 'feFuncG'
-  | 'feFuncR'
-  | 'feGaussianBlur'
-  | 'feImage'
-  | 'feMerge'
-  | 'feMergeNode'
-  | 'feMorphology'
-  | 'feOffset'
-  | 'fePointLight'
-  | 'feSpecularLighting'
-  | 'feSpotLight'
-  | 'feTile'
-  | 'feTurbulence'
-  | 'filter'
-  | 'foreignObject'
-  | 'g'
-  | 'image'
-  | 'line'
-  | 'linearGradient'
-  | 'marker'
-  | 'mask'
-  | 'metadata'
-  | 'path'
-  | 'pattern'
-  | 'polygon'
-  | 'polyline'
-  | 'radialGradient'
-  | 'rect'
-  | 'stop'
-  | 'svg'
-  | 'switch'
-  | 'symbol'
-  | 'text'
-  | 'textPath'
-  | 'tspan'
-  | 'use'
-  | 'view';
+  | "circle"
+  | "ellipse"
+  | "g"
+  | "line"
+  | "path"
+  | "polygon"
+  | "polyline"
+  | "rect";
 
 export type IconNode = [elementName: SVGElementType, attrs: Record<string, string>][];
 
 export interface LucideProps extends SvgProps {
   size?: string | number;
   absoluteStrokeWidth?: boolean;
-  'data-testid'?: string;
+  "data-testid"?: string;
 }
 
 export type LucideIcon = ForwardRefExoticComponent<LucideProps>;
