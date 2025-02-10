4a3134b54cea9fb0dee0a08f60feca70111e1889
Eric Fennis <eric.fennis@gmail.com>
2025-02-10 14:13:52 +0100

    ci: Improve build speeds (#2778)
    * Revert sync to async functions
    
    * Replace more sync fs functions
    
    * Format files
    
    * Fix build svelte package

--- tools/build-helpers/src/readAllMetadata.mjs
+++ tools/build-helpers/src/readAllMetadata.mjs
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import { readMetadata } from './readMetadata.mjs';
 
@@ -9,11 +9,13 @@ import { readMetadata } from './readMetadata.mjs';
  * @param {string} directory
  * @returns {object} A map of icon or category metadata
  */
-export const readAllMetadata = (directory) =>
-  fs
-    .readdirSync(directory)
+export const readAllMetadata = async (directory) => {
+  const directoryContent = await fs.readdir(directory);
+
+  return directoryContent
     .filter((file) => path.extname(file) === '.json')
     .reduce((acc, fileName) => {
       acc[path.basename(fileName, '.json')] = readMetadata(fileName, directory);
       return acc;
     }, {});
+};
