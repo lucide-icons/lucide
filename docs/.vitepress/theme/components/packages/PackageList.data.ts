230726079d6ff5ed3912504528843acf99c5e134
Moustapha HappyDev <moustapha.happydev@gmail.com>
2025-03-25 10:43:00 +0000

    apply @dotspencer's review
    resolve correct link to package docs page and github source

--- docs/.vitepress/theme/components/packages/PackageList.data.ts
+++ docs/.vitepress/theme/components/packages/PackageList.data.ts
@@ -1,24 +1,28 @@
-import packageData from '../../../data/packageData.json';
-import thirdPartyPackages from '../../../data/packageData.thirdParty.json';
-import fetchPackages from '../../../lib/fetchPackages';
+import packageDataList from "../../../data/packageData.json";
+import thirdPartyPackages from "../../../data/packageData.thirdParty.json";
+import fetchPackages from "../../../lib/fetchPackages";
 
 export default {
   async load() {
-    const packages = await fetchPackages();
+    const packageJsonList = await fetchPackages();
 
     return {
-      packages: packages
-        .filter((p) => p?.name != null && p.name in packageData)
-        .map((pData) => ({
-          ...pData,
-          ...packageData[pData.name],
-          documentation: `/guide/packages/${pData.name}`,
-          source: `https://github.com/lucide-icons/lucide/tree/main/packages/${pData.name}`,
-          icon: `/framework-logos/${packageData[pData.name].icon}.svg`,
-          iconDark: Boolean(packageData[pData.name].iconDark)
-            ? `/framework-logos/${packageData[pData.name].iconDark}.svg`
-            : null,
-        }))
+      packages: packageJsonList
+        .filter((pJson) => pJson?.name != null && pJson.name in packageDataList)
+        .map((pJson) => {
+          const packageData = packageDataList[pJson.name];
+          return {
+            ...pJson,
+            ...packageData,
+            documentation: `/guide/packages/${packageData.docsAlias ?? packageData.name}`,
+            source:
+              `https://github.com/lucide-icons/lucide/tree/main/packages/${packageData.packageDirname ?? packageData.name}`,
+            icon: `/framework-logos/${packageData.icon}.svg`,
+            iconDark: Boolean(packageData.iconDark)
+              ? `/framework-logos/${packageData.iconDark}.svg`
+              : null,
+          };
+        })
         .sort((a, b) => a.order - b.order),
       thirdPartyPackages,
     };
