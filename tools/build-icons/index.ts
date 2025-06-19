13fc99f80d16971a7a343c4ef7057483f7ad9713
Eric Fennis <eric.fennis@gmail.com>
2025-06-19 16:39:23 +0200

    fix(docs): Add TS extention to index file

--- tools/build-icons/index.ts
+++ tools/build-icons/index.ts
@@ -1,3 +1,3 @@
-export { default as getAliases } from './utils/getAliases';
-export { default as getIconMetaData } from './utils/getIconMetaData';
-export { default as renderIconsObject } from './render/renderIconsObject';
+export { default as getAliases } from './utils/getAliases.ts';
+export { default as getIconMetaData } from './utils/getIconMetaData.ts';
+export { default as renderIconsObject } from './render/renderIconsObject.ts';
