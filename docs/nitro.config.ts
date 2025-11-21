10f755d800b2dbdc15395f462af15a4ba5358f48
Eric Fennis <eric.fennis@gmail.com>
2025-11-21 14:03:13 +0100

    try this

--- docs/nitro.config.ts
+++ docs/nitro.config.ts
@@ -2,29 +2,29 @@ import copy from 'rollup-plugin-copy';
 import replace from '@rollup/plugin-replace';
 
 export default defineNitroConfig({
-  compatibilityDate: '2025-07-30',
+  compatibilityDate: '2025-21-11',
   preset: 'vercel_edge',
   srcDir: '.vitepress',
   routeRules: {
     '/api/**': { cors: false },
   },
   rollupConfig: {
-    external: ['@resvg/resvg-wasm/index_bg.wasm', './index_bg.wasm?module'],
+    // external: ['@resvg/resvg-wasm/index_bg.wasm', './index_bg.wasm?module'],
     plugins: [
-      copy({
-        targets: [
-          {
-            src: './node_modules/@resvg/resvg-wasm/index_bg.wasm',
-            dest: './.vercel/output/functions/__nitro.func',
-          },
-        ],
-      }),
-      replace({
-        include: ['./**/*.ts'],
-        '/* WASM_IMPORT */': 'import resvg_wasm from "./index_bg.wasm?module";',
-        delimiters: ['', ''],
-        preventAssignment: false,
-      }),
+      // copy({
+      //   targets: [
+      //     {
+      //       src: './node_modules/@resvg/resvg-wasm/index_bg.wasm',
+      //       dest: './.vercel/output/functions/__nitro.func',
+      //     },
+      //   ],
+      // }),
+      // replace({
+      //   include: ['./**/*.ts'],
+      //   '/* WASM_IMPORT */': 'import resvg_wasm from "./index_bg.wasm?module";',
+      //   delimiters: ['', ''],
+      //   preventAssignment: false,
+      // }),
     ],
   },
   esbuild: {
