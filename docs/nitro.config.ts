fa7408d46c3cea914ed44a64ae9e296ec3577ced
Eric Fennis <eric.fennis@gmail.com>
2025-11-21 16:01:28 +0100

    Revert import change

--- docs/nitro.config.ts
+++ docs/nitro.config.ts
@@ -9,22 +9,22 @@ export default defineNitroConfig({
     '/api/**': { cors: false },
   },
   rollupConfig: {
-    // external: ['@resvg/resvg-wasm/index_bg.wasm', './index_bg.wasm?module'],
+    external: ['@resvg/resvg-wasm/index_bg.wasm', './index_bg.wasm?module'],
     plugins: [
-      // copy({
-      //   targets: [
-      //     {
-      //       src: './node_modules/@resvg/resvg-wasm/index_bg.wasm',
-      //       dest: './.vercel/output/functions/__nitro.func',
-      //     },
-      //   ],
-      // }),
-      // replace({
-      //   include: ['./**/*.ts'],
-      //   '/* WASM_IMPORT */': 'import resvg_wasm from "./index_bg.wasm?module";',
-      //   delimiters: ['', ''],
-      //   preventAssignment: false,
-      // }),
+      copy({
+        targets: [
+          {
+            src: './node_modules/@resvg/resvg-wasm/index_bg.wasm',
+            dest: './.vercel/output/functions/__fallback.func',
+          },
+        ],
+      }),
+      replace({
+        include: ['./**/*.ts'],
+        '/* WASM_IMPORT */': 'import resvg_wasm from "./index_bg.wasm?module";',
+        delimiters: ['', ''],
+        preventAssignment: false,
+      }),
     ],
   },
   esbuild: {
