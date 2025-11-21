e3cde71dc278f03caa429a8435af3e1fdf376d78
Eric Fennis <eric.fennis@gmail.com>
2025-11-21 12:03:00 +0100

    update deps

--- tools/rollup-plugins/plugins.js
+++ tools/rollup-plugins/plugins.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { visualizer } from 'rollup-plugin-visualizer';
-import bundleSize from '@atomico/rollup-plugin-sizes';
 import replace from '@rollup/plugin-replace';
 import license from 'rollup-plugin-license';
 import esbuild from 'rollup-plugin-esbuild';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import summary from "rollup-plugin-summary";
 
-const plugins = ({ pkg, minify = false, withEsbuild = true, esbuildOptions = {} }) =>
+const plugins = ({ pkg, minify = false, withEsbuild = true, esbuildOptions = {}, withSummary = false }) =>
   [
     withEsbuild
       ? esbuild({
@@ -24,13 +24,13 @@ const plugins = ({ pkg, minify = false, withEsbuild = true, esbuildOptions = {} 
 This source code is licensed under the ${pkg.license} license.
 See the LICENSE file in the root directory of this source tree.`,
     }),
-    bundleSize(),
+    withSummary ? summary() : null,
     visualizer({
       sourcemap: true,
       filename: `stats/${pkg.name}${minify ? '-min' : ''}.html`,
     }),
   ].filter(Boolean);
 
-export { bundleSize, license, visualizer, replace };
+export { license, visualizer, replace };
 
 export default plugins;
