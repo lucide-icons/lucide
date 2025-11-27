594fa37b0274878e7ea1556cbd433d661c50159e
Eric Fennis <eric.fennis@gmail.com>
2025-11-27 10:23:55 +0100

    Revert nitro config change

--- docs/nitro.config.ts
+++ docs/nitro.config.ts
@@ -2,20 +2,20 @@ import copy from 'rollup-plugin-copy';
 import replace from '@rollup/plugin-replace';
 
 export default defineNitroConfig({
-  compatibilityDate: '2025-11-21',
-  preset: 'vercel-edge',
+  compatibilityDate: '2025-07-30',
+  preset: 'vercel_edge',
   srcDir: '.vitepress',
   routeRules: {
     '/api/**': { cors: false },
   },
   rollupConfig: {
-    external: ['@resvg/resvg-wasm/index_bg.wasm', './index_bg.wasm', './index_bg.wasm?module'],
+    external: ['@resvg/resvg-wasm/index_bg.wasm', './index_bg.wasm?module'],
     plugins: [
       copy({
         targets: [
           {
             src: './node_modules/@resvg/resvg-wasm/index_bg.wasm',
-            dest: './.vercel/output/functions/__fallback.func',
+            dest: './.vercel/output/functions/__nitro.func',
           },
         ],
       }),
