bb7da54e4642f5bc719cca5298287dc64fac8824
Karsa <contact@karsa.org>
2026-04-06 17:13:46 +0200

    Update CarbonAdOverlay.vue

--- docs/.vitepress/theme/components/icons/CarbonAdOverlay.vue
+++ docs/.vitepress/theme/components/icons/CarbonAdOverlay.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useData } from 'vitepress';
-import { useSessionStorage } from '@vueuse/core';
+import { StorageSerializers, useSessionStorage } from '@vueuse/core';
 import IconButton from '../base/IconButton.vue';
 import VPDocAsideCarbonAds from 'vitepress/dist/client/theme-default/components/VPDocAsideCarbonAds.vue';
 import { x } from '../../../data/iconNodes';
@@ -8,7 +8,9 @@ import Icon from '@lucide/vue/src/Icon';
 import { computed, onMounted, ref } from 'vue';
 
 const { theme } = useData();
-const showAd = useSessionStorage('show-carbon-ads', true);
+const showAd = useSessionStorage('show-carbon-ads', true, {
+  serializer: StorageSerializers.boolean,
+});
 const carbonLoaded = ref(true);
 const shouldHideAd = computed(() => !showAd.value || !carbonLoaded.value);
 
