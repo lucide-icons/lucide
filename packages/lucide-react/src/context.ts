bae2f5e7472e531f7ec8b072f6db5d8c0fe865e4
Karsa <contact@karsa.org>
2026-04-09 11:43:08 +0200

    fix: revert changes made to lucide-react context.ts

--- packages/lucide-react/src/context.ts
+++ packages/lucide-react/src/context.ts
@@ -4,7 +4,7 @@ import { createContext, createElement, type ReactNode, useContext, useMemo } fro
 import { LucideProps } from './types';
 
 type LucideConfig = {
-  size: string | number;
+  size: number;
   color: string;
   strokeWidth: number;
   absoluteStrokeWidth: boolean;
