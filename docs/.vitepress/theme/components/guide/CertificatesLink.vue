20c7ce7c9161c78940848d82bd6ca323e6074dc8
Eric Fennis <eric.fennis@gmail.com>
2026-05-15 14:26:51 +0200

    Add cert link component

---
+++ docs/.vitepress/theme/components/guide/CertificatesLink.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="certificates-link">
+    test
+  </div>
+</template>
+
+<style scoped lang="css">
+.certificates-link {
+  margin: 2px 0;
+  background-color: var(--vt-c-white-soft);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-radius: 2px;
+  transition: background-color 0.2s ease;
+  height: calc(var(--max-width) / 2 - 6px);
+}
+</style>
