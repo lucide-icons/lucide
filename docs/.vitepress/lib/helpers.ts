61ad24b2171b5d66dbc74f727ed9e387f130e290
Eric Fennis <eric.fennis@gmail.com>
2026-08-06 12:05:54 +0200

    Fix eslint errors

--- docs/.vitepress/lib/helpers.ts
+++ docs/.vitepress/lib/helpers.ts
@@ -9,11 +9,11 @@ export const hash = (string: string, seed = 5381) => {
   let i = string.length;
 
   while (i) {
-    // eslint-disable-next-line no-bitwise, no-plusplus
+     
     seed = (seed * 33) ^ string.charCodeAt(--i);
   }
 
-  // eslint-disable-next-line no-bitwise
+   
   return (seed >>> 0).toString(36).substr(0, 6);
 };
 
